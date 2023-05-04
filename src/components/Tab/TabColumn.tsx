e106b8b3019f91b7b4b08127209ddd62720324ab
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-05-04 18:51:46 -0500

    extra props to handle palm muting sections

--- src/components/Tab/TabColumn.tsx
+++ src/components/Tab/TabColumn.tsx
@@ -1,12 +1,18 @@
 import React, { Fragment } from "react";
 import { type ITabSection } from "./Tab";
 import TabNote from "./TabNote";
+import PalmMuteNode from "./PalmMuteNode";
 
 interface TabColumn {
   columnData: string[];
   setTabData: React.Dispatch<React.SetStateAction<ITabSection[]>>;
   sectionIndex: number;
   columnIndex: number;
+  editing: boolean;
+  addingNewPalmMuteSection: boolean;
+  setAddingNewPalmMuteSection: React.Dispatch<React.SetStateAction<boolean>>;
+  newPalmMuteLocation: number[];
+  setNewPalmMuteLocation: React.Dispatch<React.SetStateAction<number[]>>;
 }
 
 function TabColumn({
@@ -14,50 +20,73 @@ function TabColumn({
   setTabData,
   sectionIndex,
   columnIndex,
+  // finalColumnInSection, add later
+  editing,
+  addingNewPalmMuteSection,
+  setAddingNewPalmMuteSection,
+  newPalmMuteLocation,
+  setNewPalmMuteLocation,
 }: TabColumn) {
   // need handling for when column is a measure line
 
   return (
-    <div className="baseVertFlex gap-2">
+    <div
+      style={{
+        gap: `${columnIndex % 2 === 0 ? "0.5rem" : "0rem"}`,
+      }}
+      className="baseVertFlex my-4"
+    >
       {columnData.map((note, index) => (
         <Fragment key={index}>
           {index === 0 && (
             <div className="baseVertFlex w-full">
-              <TabNote
+              <PalmMuteNode
                 note={note}
-                setTabData={setTabData}
-                sectionIndex={sectionIndex}
                 columnIndex={columnIndex}
-                noteIndex={index}
+                setTabData={setTabData}
+                addingNewPalmMuteSection={addingNewPalmMuteSection}
+                setAddingNewPalmMuteSection={setAddingNewPalmMuteSection}
+                newPalmMuteLocation={newPalmMuteLocation}
+                setNewPalmMuteLocation={setNewPalmMuteLocation}
               />
+
               <div className="mt-4 h-[2px] w-full bg-pink-50"></div>
             </div>
           )}
 
           {index > 0 && index < 7 && (
             <div className="baseFlex">
-              <div className="h-[1px] w-4 bg-pink-50"></div>
+              {/* maybe w-4 needs to go down to w-2 on odd idx? */}
+              {columnIndex % 2 === 0 && (
+                <div className="h-[1px] w-4 bg-pink-50"></div>
+              )}
               <TabNote
                 note={note}
                 setTabData={setTabData}
                 sectionIndex={sectionIndex}
                 columnIndex={columnIndex}
                 noteIndex={index}
+                editing={editing}
               />
-              <div className="h-[1px] w-4 bg-pink-50"></div>
+              {columnIndex % 2 === 0 && (
+                <div className="h-[1px] w-4 bg-pink-50"></div>
+              )}
             </div>
           )}
 
           {index === 7 && (
             <div className="baseVertFlex w-full">
               <div className="mb-4 h-[2px] w-full bg-pink-50"></div>
-              <TabNote
-                note={note}
-                setTabData={setTabData}
-                sectionIndex={sectionIndex}
-                columnIndex={columnIndex}
-                noteIndex={index}
-              />
+              {columnIndex % 2 === 0 && (
+                <TabNote
+                  note={note}
+                  setTabData={setTabData}
+                  sectionIndex={sectionIndex}
+                  columnIndex={columnIndex}
+                  noteIndex={index}
+                  editing={editing}
+                />
+              )}
             </div>
           )}
         </Fragment>
