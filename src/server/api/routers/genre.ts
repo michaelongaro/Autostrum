83e2746b9c120c44b73ed367823ec0a2dd23db25
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-05-19 23:27:51 -0500

    replaced example router with genre router

---
+++ src/server/api/routers/genre.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const genreRouter = createTRPCRouter({
+  // hello: publicProcedure
+  //   .input(z.object({ text: z.string() }))
+  //   .query(({ input }) => {
+  //     return {
+  //       greeting: `Hello ${input.text}`,
+  //     };
+  //   }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.genre.findMany();
+  }),
+});
