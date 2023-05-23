7cbbf4513bd5266bc5a5c502fcb1cad5b63475c1
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-05-22 23:57:07 -0500

    uses drop-shadow instead of box-shadow

--- src/components/Tab/TabMeasureLine.tsx
+++ src/components/Tab/TabMeasureLine.tsx
@@ -23,6 +23,7 @@ const initialStyles = {
   y: 0,
   scale: 1,
   opacity: 1,
+  filter: "drop-shadow(0px 5px 5px transparent)",
 };
 
 function TabMeasureLine({ columnData, sectionIndex, columnIndex }: TabColumn) {
@@ -69,9 +70,9 @@ function TabMeasureLine({ columnData, sectionIndex, columnIndex }: TabColumn) {
               opacity: 1,
               scale: isDragging ? 1.05 : 1,
               zIndex: isDragging ? 1 : 0,
-              boxShadow: isDragging
-                ? "0 0 0 1px rgba(63, 63, 68, 0.05), 0px 15px 15px 0 rgba(34, 33, 81, 0.25)"
-                : undefined,
+              filter: isDragging
+                ? "drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.25)"
+                : "drop-shadow(0px 5px 5px transparent)",
             }
           : initialStyles
       }
--- src/components/modals/SectionProgressionModal.tsx
+++ src/components/modals/SectionProgressionModal.tsx
@@ -283,6 +283,7 @@ const initialStyles = {
   y: 0,
   scale: 1,
   opacity: 1,
+  filter: "drop-shadow(0px 5px 5px transparent)",
 };
 
 interface Section {
@@ -382,9 +383,9 @@ function Section({ id, title, repetitions, index, titles }: Section) {
               opacity: 1,
               scale: isDragging ? 1.05 : 1,
               zIndex: isDragging ? 1 : 0,
-              boxShadow: isDragging
-                ? "0 0 0 1px rgba(63, 63, 68, 0.05), 0px 15px 15px 0 rgba(34, 33, 81, 0.25)"
-                : undefined,
+              filter: isDragging
+                ? "drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.25)"
+                : "drop-shadow(0px 5px 5px transparent)",
             }
           : initialStyles
       }
