456c29e7e5f93c284240a286fd51745e3d1caf32
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-05-24 21:51:24 -0500

    init commit

---
+++ src/components/modals/EffectGlossaryModal.tsx
@@ -0,0 +1,78 @@
+import { useRef, useEffect } from "react";
+import { useTabStore } from "~/stores/TabStore";
+import { shallow } from "zustand/shallow";
+import { motion } from "framer-motion";
+
+import EffectGlossary from "../ui/EffectGlossary";
+
+const backdropVariants = {
+  expanded: {
+    opacity: 1,
+  },
+  closed: {
+    opacity: 0,
+  },
+};
+
+function EffectGlossaryModal() {
+  const innerModalRef = useRef<HTMLDivElement>(null);
+
+  const { showEffectGlossaryModal, setShowEffectGlossaryModal } = useTabStore(
+    (state) => ({
+      showEffectGlossaryModal: state.showEffectGlossaryModal,
+      setShowEffectGlossaryModal: state.setShowEffectGlossaryModal,
+    }),
+    shallow
+  );
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (
+        showEffectGlossaryModal &&
+        innerModalRef.current &&
+        !innerModalRef.current.contains(event.target as Node)
+      ) {
+        setShowEffectGlossaryModal(false);
+      }
+    };
+
+    // not sure exactly why timeout is needed. This was being run seemingly before this component was
+    // mounted.
+    setTimeout(
+      () => document.addEventListener("click", handleClickOutside),
+      250
+    );
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [showEffectGlossaryModal, setShowEffectGlossaryModal]);
+
+  return (
+    <motion.div
+      key={"EffectGlossaryModalBackdrop"}
+      className="baseFlex fixed left-0 top-0 z-50 h-[100vh] w-[100vw] bg-black/50"
+      variants={backdropVariants}
+      initial="closed"
+      animate="expanded"
+      exit="closed"
+      onClick={() => {
+        console.log("hi");
+        setShowEffectGlossaryModal(false);
+      }}
+    >
+      <div
+        ref={innerModalRef}
+        style={{
+          height: "485px", // height of EffectGlossary
+          // not sure why we need this... but it wasn't centered horizontally automatically
+          width: "60px",
+        }}
+      >
+        <EffectGlossary forModal />
+      </div>
+    </motion.div>
+  );
+}
+
+export default EffectGlossaryModal;
---
+++ src/components/ui/EffectGlossary.tsx
@@ -0,0 +1,134 @@
+import { useTabStore } from "~/stores/TabStore";
+import { shallow } from "zustand/shallow";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./dropdownMenu";
+
+interface EffectGlossary {
+  forModal?: boolean;
+}
+
+interface Styles {
+  padding: string;
+  width: string;
+  height: string;
+  border?: string;
+}
+
+function EffectGlossary({ forModal = false }: EffectGlossary) {
+  const { showingEffectGlossary, setShowingEffectGlossary } = useTabStore(
+    (state) => ({
+      showingEffectGlossary: state.showingEffectGlossary,
+      setShowingEffectGlossary: state.setShowingEffectGlossary,
+    }),
+    shallow
+  );
+
+  const style: Styles = {
+    padding: forModal ? "0" : "0.5rem 1rem",
+    width: forModal ? "0" : "auto",
+    height: forModal ? "0" : "2.5rem",
+  };
+
+  if (forModal) {
+    style.border = "none";
+  }
+
+  return (
+    <DropdownMenu
+      onOpenChange={(open) => {
+        setShowingEffectGlossary(open ? true : false);
+      }}
+      open={forModal ? true : showingEffectGlossary}
+    >
+      <DropdownMenuTrigger
+        // not ideal, I feel like you can change source code to make DropdownMenuTrigger return a div
+        // instead of a button. This was getting angry that there was a button within a button hydration-wise
+        style={style}
+        className="baseFlex lightGlassmorphic active:ring-offset-background~ ml-8 inline-flex h-10 items-center justify-center gap-4 rounded-md border-2 px-4 py-2 text-sm font-medium text-secondary-foreground ring-offset-background transition-all hover:bg-secondary/80 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:ring-0 active:ring-offset-0 active:brightness-75 disabled:pointer-events-none disabled:opacity-50"
+      >
+        {!forModal && (
+          <>
+            Effect glossary
+            <span>{showingEffectGlossary ? "-" : "+"}</span>
+          </>
+        )}
+      </DropdownMenuTrigger>
+
+      {/* doesn't look cohesive when below styles are present, any other solution besides changing
+          all other dropdown-esque ui components to match this? */}
+      {/* className="heavyGlassmorphic text-pink-900" */}
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Section effects</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">PM</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Palm mute</span>
+        </DropdownMenuItem>
+        <DropdownMenuLabel>Note effects</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {/* maybe add O and o for clarity on each */}
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">|</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Measure line</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">x</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Muted note</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">h</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Hammer on</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">p</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Pull off</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">/</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Slide up</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">\</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Slide down</span>
+        </DropdownMenuItem>
+        <DropdownMenuLabel>Chord effects</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">^</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Strum up</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">v</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Strum down</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">s</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Slap</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="grid w-48 grid-cols-5">
+          <span className="col-span-1">.</span>
+          <span className="col-span-1">-</span>
+          <span className="col-span-3">Stacatto</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default EffectGlossary;
