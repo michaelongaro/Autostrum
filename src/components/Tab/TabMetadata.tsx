51fac818dacf84265da27bf66018e3d9332a43f1
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-05-29 01:24:00 -0500

    string formatting utility functions

--- src/components/Tab/TabMetadata.tsx
+++ src/components/Tab/TabMetadata.tsx
@@ -173,6 +173,33 @@ function TabMetadata() {
     return isEqual(sanitizedOriginalTabData, sanitizedCurrentTabData);
   }
 
+  function getOrdinalSuffix(num: number) {
+    const j = num % 10,
+      k = num % 100;
+    if (j === 1 && k !== 11) {
+      return `${num}st`;
+    }
+    if (j === 2 && k !== 12) {
+      return `${num}nd`;
+    }
+    if (j === 3 && k !== 13) {
+      return `${num}rd`;
+    }
+    return `${num}th`;
+  }
+
+  function formatDate(date: Date) {
+    const d = new Date(date);
+    let month = ` ${d.getMonth() + 1}`;
+    let day = `  ${d.getDate()}`;
+    const year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [month, day, year].join(" - ");
+  }
+
   return (
     <>
       {editing ? (
