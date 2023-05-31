db82c9c7b9f9c0e3342c2197b21c7b77545171ca
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-05-31 16:58:37 -0500

    added tabRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { genreRouter } from "~/server/api/routers/genre";
+import { tabRouter } from "./routers/tab";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { genreRouter } from "~/server/api/routers/genre";
  */
 export const appRouter = createTRPCRouter({
   genre: genreRouter,
+  tab: tabRouter,
 });
 
 // export type definition of API
