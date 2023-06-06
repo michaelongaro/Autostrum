5dcf1397cb4bed0ed7a6ab3cdba277dece9052b7
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-06-06 04:02:48 -0500

    able to query based on id or username

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -4,10 +4,24 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  getUserById: publicProcedure.input(z.string()).query(async ({ input }) => {
-    const [user] = await clerkClient.users.getUserList({
-      userId: [input],
-    });
-    return user;
-  }),
+  getUserByIdOrUsername: publicProcedure
+    .input(
+      z.object({
+        id: z.string().optional(),
+        username: z.string().optional(),
+      })
+    )
+    .query(async ({ input }) => {
+      if (input.id) {
+        const [user] = await clerkClient.users.getUserList({
+          userId: [input.id],
+        });
+        return user;
+      } else if (input.username) {
+        const [user] = await clerkClient.users.getUserList({
+          username: [input.username],
+        });
+        return user;
+      }
+    }),
 });
