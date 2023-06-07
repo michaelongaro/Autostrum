c971575de36663c4aa7f7b431c22359a03e80407
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-06-07 03:43:26 -0500

    header is now fixed position instead of absolute

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import MobileHeader from "./MobileHeader";
 
 function Header() {
   return (
-    <nav className="heavyGlassmorphic absolute left-0 top-0 z-10 grid h-16 w-full grid-cols-1 grid-rows-1 ">
+    <nav className="heavyGlassmorphic fixed left-0 top-0 z-[999] grid h-16 w-full grid-cols-1 grid-rows-1 ">
       <DesktopHeader />
       <MobileHeader />
     </nav>
