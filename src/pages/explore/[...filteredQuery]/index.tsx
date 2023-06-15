7d8e008b01bbd880165fd91de6c7722b1c6470f4
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-06-15 03:47:14 -0500

    most of useEffect moved to parent layout + simplified

--- src/pages/explore/[...filteredQuery]/index.tsx
+++ src/pages/explore/[...filteredQuery]/index.tsx
@@ -19,122 +19,33 @@ function FilteredQueryExplore() {
 
   // view type needs to be on user metadata and will be updated whenever user changes it to the other setting
   // obv means that you will need to query user but that's totally fine
+  // reminder that for /artist/username/[...slug] index.tsx page it will basically
+  // be the same, just adding <ArtistProfile /> above and then the only restriction
+  // is that instead of genreId and type being mutually exclusive, type is simply
+  // just not allowed since it is just for tabs!
 
+  // maybe useLayoutEffect if this is causing a flicker
   useEffect(() => {
     if (Object.keys(query).length === 0) return;
 
-    const { filteredQuery, ...queryObj } = query;
-
-    const validQueryKeys = [
-      "genreId",
-      "type",
-      "search",
-      "relevance",
-      "sort",
-      "view",
-    ];
-
-    for (const key of Object.keys(queryObj)) {
-      if (!validQueryKeys.includes(key)) {
-        // TODO: most likely move this logic one step up to the ExploreLayout
-        // component so that we can just redirect to 404 page from there rather
-        // than still rendering the input and everything, just conditionally render
-        // a rough 404 blurb and then don't render children if that's the case
-        setServe404Page(true);
-        return;
-      }
-    }
-
-    for (const [key, value] of Object.entries(queryObj)) {
-      switch (key) {
-        case "genreId":
-          if (typeof value === "string" && isNaN(parseInt(value))) {
-            setServe404Page(true);
-            return;
-          }
-          setGenreId(parseInt(value as string));
-          break;
-        case "type":
-          if (
-            typeof value === "string" &&
-            value !== "tabs" &&
-            value !== "artists"
-          ) {
-            setServe404Page(true);
-            return;
-          }
-          setType(value as "tabs" | "artists");
-          break;
-        case "search":
-          if (typeof value === "string") {
-            setSearchQuery(value);
-          }
-          break;
-        case "relevance":
-          if (
-            typeof value === "string" &&
-            value !== "true" &&
-            value !== "false"
-          ) {
-            setServe404Page(true);
-            return;
-          }
-          setSortByRelevance(value === "true" ? true : false);
-          break;
-        case "sort":
-          if (
-            typeof value === "string" &&
-            value !== "newest" &&
-            value !== "oldest" &&
-            value !== "leastLiked" &&
-            value !== "mostLiked"
-          ) {
-            setServe404Page(true);
-            return;
-          }
-          setAdditionalSortFilter(
-            value as "newest" | "oldest" | "leastLiked" | "mostLiked"
-          );
-          break;
-        case "view":
-          if (
-            typeof value === "string" &&
-            value !== "grid" &&
-            value !== "table"
-          ) {
-            setServe404Page(true);
-            return;
-          }
-          setViewType(value as "grid" | "table");
-          break;
-      }
-    }
-
-    // reminder that for /artist/username/[...slug] index.tsx page it will basically
-    // be the same, just adding <ArtistProfile /> above and then the only restriction
-    // is that instead of genreId and type being mutually exclusive, type is simply
-    // just not allowed since it is just for tabs!
-
-    // another reminder: we control all of the url structure internally, so anything
-    // that doesn't match is most definitely a 404 page!
-  }, [query]); // or query if suits needs better
+    setGenreId(parseInt(query.genreId as string) || undefined);
+    setType((query.type as "tabs" | "artists") || undefined);
+    setSearchQuery((query.search as string) || undefined);
+    setSortByRelevance(query.relevance === "true" ? true : false);
+    setAdditionalSortFilter(
+      (query.sort as "newest" | "oldest" | "leastLiked" | "mostLiked") ||
+        undefined
+    );
+    setViewType(query.view as "grid" | "table");
+  }, [query]);
 
   // longest example url
   // http://localhost:3000/explore/filters?genreId=1&search=sun%20shine&relevance=true&sort=newest&view=grid
 
   // def create util function to create url from query params (use encodeURIComponent('sun shine'))
 
-  // how to properly format urls (query params + slashes etc)
-
   // could be proper way to change browser url w/o reloading page
   // router.push('/?counter=10', undefined, { shallow: true })
-
-  console.log("genreId", genreId);
-  console.log("type", type);
-  console.log("searchQuery", searchQuery);
-  console.log("sortByRelevance", sortByRelevance);
-  console.log("additionalSortFilter", additionalSortFilter);
-  console.log("viewType", viewType);
 
   return (
     <motion.div
@@ -150,16 +61,14 @@ function FilteredQueryExplore() {
         // placeholder div for now
         <div className="baseVertFlex h-full w-full"></div>
       ) : (
-        // <SearchResults
-        //   genreId={genreId}
-        //   type={type}
-        //   searchQuery={searchQuery}
-        //   sortByRelevance={sortByRelevance}
-        //   additionalSortFilter={additionalSortFilter}
-        //   viewType={viewType}
-
-        // />
-        <div>test</div>
+        <SearchResults
+          genreId={genreId}
+          type={type}
+          searchQuery={searchQuery}
+          sortByRelevance={sortByRelevance}
+          additionalSortFilter={additionalSortFilter}
+          viewType={viewType}
+        />
       )}
     </motion.div>
   );
