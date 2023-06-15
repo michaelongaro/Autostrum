3989abc2d32d51984c8765f91ebc7c25a043b37e
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-06-15 03:45:59 -0500

    first draft of filter controls + logic

--- src/components/Explore/SearchResults.tsx
+++ src/components/Explore/SearchResults.tsx
@@ -1,6 +1,21 @@
+import { useMemo } from "react";
 import { useRouter } from "next/router";
-import React from "react";
 import { api } from "~/utils/api";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
+import { AnimatePresence, motion } from "framer-motion";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+} from "../ui/select";
+import type { Genre } from "@prisma/client";
+import { BsArrowDownShort, BsGridFill } from "react-icons/bs";
+import { MdTableRows } from "react-icons/md";
 
 interface SearchResults {
   genreId?: number;
@@ -31,27 +46,27 @@ function SearchResults({
   additionalSortFilter,
   viewType,
 }: SearchResults) {
-  const { asPath } = useRouter();
+  const { asPath, push, query, pathname } = useRouter();
 
   // memo to set store filter values to query params if they aren't the same
   // may need to combine with setting store values to null initially so you don't get
   // weird behavior
 
   // do query below based on searchQuery + filters (should return either tabs[] or users[])
-  const { data: tabResults, isLoading: isLoadingTabResults } =
-    api.tab.getInfiniteTabsBySearchQuery.useInfiniteQuery(
-      // TODO: fix input types on trpc function
-      {
-        searchQuery,
-        genreId,
-        sortByRelevance,
-        sortBy,
-      },
-      {
-        enabled: type === "tab",
-        getNextPageParam: (lastPage) => lastPage.nextCursor,
-      }
-    );
+  // const { data: tabResults, isLoading: isLoadingTabResults } =
+  //   api.tab.getInfiniteTabsBySearchQuery.useInfiniteQuery(
+  //     // TODO: fix input types on trpc function
+  //     {
+  //       searchQuery,
+  //       genreId,
+  //       sortByRelevance,
+  //       sortBy,
+  //     },
+  //     {
+  //       enabled: type === "tab",
+  //       getNextPageParam: (lastPage) => lastPage.nextCursor,
+  //     }
+  //   );
 
   // const { data: artistResults, isLoading: isLoadingArtistResults } =
   //   api.user.getUsersBySearch.useInfiniteQuery(filterType === "artist" ? {
@@ -62,12 +77,358 @@ function SearchResults({
 
   // worry about skeletons later!
 
+  const genreArray = api.genre.getAll.useQuery();
+
+  const genreArrayData = useMemo(() => {
+    if (!genreArray.data) return [];
+
+    if (genreArray.data.length === 8) {
+      genreArray.data.push({
+        id: 9,
+        name: "All genres",
+        color: "#000000", // temp color
+      });
+    }
+
+    return genreArray.data;
+  }, [genreArray.data]);
+
+  function handleTypeChange(type: "tabs" | "artists") {
+    void push(
+      {
+        pathname,
+        query: {
+          ...query,
+          type,
+        },
+      },
+      undefined,
+      {
+        scroll: false, // defaults to true but try both
+        shallow: true,
+      }
+    );
+  }
+
+  // &filteredQuery=filters gets added onto end of url...
+
+  function handleGenreChange(stringifiedId: string) {
+    void push(
+      {
+        pathname,
+        query: {
+          ...query,
+          genreId: stringifiedId,
+        },
+      },
+      undefined,
+      {
+        scroll: false, // defaults to true but try both
+        shallow: true,
+      }
+    );
+  }
+
+  function handleViewChange(viewType: "grid" | "table") {
+    void push(
+      {
+        pathname,
+        query: {
+          ...query,
+          view: viewType,
+        },
+      },
+      undefined,
+      {
+        scroll: false, // defaults to true but try both
+        shallow: true,
+      }
+    );
+  }
+
+  function handleRelevanceChange() {
+    void push(
+      {
+        pathname,
+        query: {
+          ...query,
+          relevance: query.relevance === "true" ? false : true,
+        },
+      },
+      undefined,
+      {
+        scroll: false, // defaults to true but try both
+        shallow: true,
+      }
+    );
+  }
+
+  // ask chatgpt to clean this logic up for you
+  function handleSortChange(type: "date" | "likes") {
+    let newSortParam = "";
+
+    switch (query.sort) {
+      case "newest":
+        if (type === "date") {
+          newSortParam = "oldest";
+        } else {
+          newSortParam = "mostLiked";
+        }
+        break;
+      case "oldest":
+        if (type === "date") {
+          newSortParam = "";
+        } else {
+          newSortParam = "mostLiked";
+        }
+        break;
+      case "mostLiked":
+        if (type === "likes") {
+          newSortParam = "leastLiked";
+        } else {
+          newSortParam = "newest";
+        }
+        break;
+      case "leastLiked":
+        if (type === "likes") {
+          newSortParam = "";
+        } else {
+          newSortParam = "newest";
+        }
+        break;
+      case undefined:
+        if (type === "date") {
+          newSortParam = "newest";
+        } else {
+          newSortParam = "mostLiked";
+        }
+        break;
+    }
+
+    const newQueries = { ...query };
+
+    if (newSortParam === "") delete newQueries.sort;
+    else newQueries.sort = newSortParam;
+
+    // push new url to router
+    void push(
+      {
+        pathname,
+        query: newQueries,
+      },
+      undefined,
+      {
+        scroll: false, // defaults to true but try both
+        shallow: true,
+      }
+    );
+  }
+
+  console.log(additionalSortFilter);
+
   return (
     // prob better practice to let parent component decide vertical margin/padding
     // rather than do it here.
-    <div className="baseVertFlex w-full rounded-md border-2 border-pink-950">
+    <div className="baseVertFlex mt-8 w-full rounded-md border-2 border-pink-900">
       {/* # of results + sorting options */}
-      <div className="baseVertFlex w-full bg-pink-950 px-4 py-8 md:flex-row md:!justify-between"></div>
+      <div className="baseVertFlex w-full bg-pink-900 px-4 py-2 md:flex-row md:!justify-between">
+        {/* # of results */}
+        {/* tabResults.length */}
+        {searchQuery && (
+          <p className="text-lg">{`${15} results for "${searchQuery}"`}</p>
+        )}
+        {/* most likely need another block for if searchQuery is undefined where it will show like
+            "All {genre} tabs" or "All artists". Think about it */}
+
+        {/* sorting options */}
+        <div className="baseFlex gap-2 md:gap-4">
+          {/* type (artist page only shows tabs so hide selector) */}
+          {!asPath.includes("/artist") && (
+            <div className="baseVertFlex !items-start gap-1.5">
+              <Label>Type</Label>
+              <div className="baseFlex gap-2">
+                <Button
+                  variant={type !== "artists" ? "toggledOn" : "toggledOff"}
+                  size="sm"
+                  onClick={() => handleTypeChange("tabs")}
+                >
+                  Tabs
+                </Button>
+                <Button
+                  variant={type === "artists" ? "toggledOn" : "toggledOff"}
+                  size="sm"
+                  onClick={() => handleTypeChange("artists")}
+                >
+                  Artists
+                </Button>
+              </div>
+            </div>
+          )}
+
+          {/* genre selector */}
+          <AnimatePresence>
+            {(asPath.includes("/artist") || type === "tabs") && (
+              <motion.div
+                key={"searchResultsGenreSelector"}
+                initial={{ opacity: 0, width: 0, scale: 0 }}
+                animate={{ opacity: 1, width: "auto", scale: 1 }}
+                exit={{ opacity: 0, width: 0, scale: 0 }}
+                transition={{
+                  scale: {
+                    duration: 0.15,
+                  },
+                  opacity: {
+                    duration: 0.25,
+                  },
+                  width: {
+                    duration: 0.25,
+                  },
+                }}
+                className="baseVertFlex !items-start gap-1.5"
+              >
+                <Label>Genre</Label>
+                <Select
+                  // not sure on logic here, maybe change this so that unless type="tabs" genreId
+                  // HAS to be defined (9 being "all genres") ?
+                  value={(genreId ?? 9).toString()}
+                  onValueChange={(value) => handleGenreChange(value)}
+                >
+                  <SelectTrigger className="w-[180px] border-2">
+                    <SelectValue placeholder="Select a genre" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Genres</SelectLabel>
+
+                      {genreArrayData.map((genre) => {
+                        return (
+                          <SelectItem
+                            key={genre.id}
+                            value={genre.id.toString()}
+                          >
+                            <div className="baseFlex gap-2">
+                              <div
+                                style={{
+                                  backgroundColor: genre.color,
+                                }}
+                                className="h-3 w-3 rounded-full"
+                              ></div>
+                              {genre.name}
+                            </div>
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
+          {/* view */}
+          <div className="baseVertFlex !items-start gap-1.5">
+            <Label>View</Label>
+            <div className="baseFlex gap-2">
+              <Button
+                variant={viewType === "grid" ? "toggledOn" : "toggledOff"}
+                size="sm"
+                onClick={() => handleViewChange("grid")}
+              >
+                <BsGridFill className="h-4 w-4" />
+              </Button>
+              <Button
+                variant={viewType === "table" ? "toggledOn" : "toggledOff"}
+                size="sm"
+                onClick={() => handleViewChange("table")}
+              >
+                <MdTableRows className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+
+          {/* relevance + extra sorting filters */}
+          <div className="baseVertFlex !items-start gap-1.5">
+            <Label>Sort by</Label>
+            <div className="baseFlex gap-2">
+              <Button
+                variant={sortByRelevance ? "toggledOn" : "toggledOff"}
+                size="sm"
+                onClick={() => handleRelevanceChange()}
+              >
+                Relevance
+              </Button>
+              <Button
+                variant={
+                  additionalSortFilter === "newest" ||
+                  additionalSortFilter === "oldest"
+                    ? "toggledOn"
+                    : "toggledOff"
+                }
+                size="sm"
+                onClick={() => handleSortChange("date")}
+              >
+                Date
+                <AnimatePresence>
+                  {(additionalSortFilter === "newest" ||
+                    additionalSortFilter === "oldest") && (
+                    <motion.div
+                      key={"searchResultsDateSortArrow"}
+                      initial={{ opacity: 0, width: 0 }}
+                      animate={{ opacity: 1, width: "auto" }}
+                      exit={{ opacity: 0, width: 0 }}
+                      transition={{ duration: 0.25 }}
+                    >
+                      <BsArrowDownShort
+                        style={{
+                          transform:
+                            additionalSortFilter === "newest"
+                              ? "rotate(0deg)"
+                              : "rotate(180deg)",
+                        }}
+                        className="h-4 w-4 transition-all"
+                      />
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </Button>
+              <Button
+                variant={
+                  additionalSortFilter === "leastLiked" ||
+                  additionalSortFilter === "mostLiked"
+                    ? "toggledOn"
+                    : "toggledOff"
+                }
+                size="sm"
+                onClick={() => handleSortChange("likes")}
+              >
+                Likes
+                <AnimatePresence>
+                  {(additionalSortFilter === "leastLiked" ||
+                    additionalSortFilter === "mostLiked") && (
+                    <motion.div
+                      key={"searchResultsLikesSortArrow"}
+                      initial={{ opacity: 0, width: 0 }}
+                      animate={{ opacity: 1, width: "auto" }}
+                      exit={{ opacity: 0, width: 0 }}
+                      transition={{ duration: 0.25 }}
+                    >
+                      <BsArrowDownShort
+                        style={{
+                          transform:
+                            additionalSortFilter === "mostLiked"
+                              ? "rotate(0deg)"
+                              : "rotate(180deg)",
+                        }}
+                        className="h-4 w-4 transition-all"
+                      />
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* card view */}
 
