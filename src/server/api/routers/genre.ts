6c8061cfda308648845eb811c238bf51dc0e62f1
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-06-21 03:33:09 -0500

    additional function to get count of tabs per genre

--- src/server/api/routers/genre.ts
+++ src/server/api/routers/genre.ts
@@ -1,6 +1,11 @@
+import type { Genre } from "@prisma/client";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export interface GenreWithTotalTabNumbers extends Genre {
+  totalTabs: number;
+}
 
 export const genreRouter = createTRPCRouter({
   // hello: publicProcedure
@@ -12,5 +17,26 @@ export const genreRouter = createTRPCRouter({
   //   }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.genre.findMany();
+  }),
+
+  getAllWithTotalTabNumbers: publicProcedure.query(async ({ ctx }) => {
+    const genres = await ctx.prisma.genre.findMany({
+      include: {
+        _count: {
+          select: {
+            tabs: true,
+          },
+        },
+      },
+    });
+
+    const genresWithTotalTabNumbers: GenreWithTotalTabNumbers[] = genres.map(
+      (genre) => ({
+        ...genre,
+        totalTabs: genre._count.tabs,
+      })
+    );
+
+    return genresWithTotalTabNumbers;
   }),
 });
