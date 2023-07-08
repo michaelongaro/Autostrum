af2b560871ece68fa5a92c8bbfc3535ebd47e9d6
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-07-08 03:29:41 -0500

    added edit + delete buttons

--- src/components/Tab/Chords.tsx
+++ src/components/Tab/Chords.tsx
@@ -2,20 +2,14 @@ import { useTabStore } from "~/stores/TabStore";
 import { shallow } from "zustand/shallow";
 import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 
 function Chords() {
-  const {
-    chords,
-    setChords,
-    setShowChordModal,
-    setCurrentlyEditingChord,
-    editing,
-  } = useTabStore(
+  const { chords, setChords, setChordThatIsBeingEdited, editing } = useTabStore(
     (state) => ({
       chords: state.chords,
       setChords: state.setChords,
-      setCurrentlyEditingChord: state.setCurrentlyEditingChord,
-      setShowChordModal: state.setShowChordModal,
+      setChordThatIsBeingEdited: state.setChordThatIsBeingEdited,
       editing: state.editing,
     }),
     shallow
@@ -26,26 +20,55 @@ function Chords() {
       <p className="text-xl font-bold">Chords</p>
       <div className="baseFlex gap-4">
         {chords.map((chord, index) => (
-          <div key={index} className="baseVertFlex gap-2">
-            <div>{chord.name}</div>
-            <div className="baseFlex">
-              {/* should have the hover tooltip below the icon */}
-              {/* should be same look as explore tab grid heart/play buttons! */}
-              <div>Edit</div>
-              <Separator orientation="vertical" className="h-8" />
-              {/* should have the hover tooltip below the icon */}
-              <div>Delete</div>
+          <div
+            key={index}
+            className="baseVertFlex border-b-none rounded-md border-2"
+          >
+            <p className="p-3 font-medium">{chord.name}</p>
+
+            <div className="baseFlex w-full !justify-evenly rounded-bl-md border-t-2">
+              {/* edit button */}
+              <Button
+                variant={"ghost"}
+                size={"sm"}
+                className="baseFlex h-8 w-1/2 gap-2 rounded-r-none rounded-bl-sm rounded-tl-none border-r-[1px]"
+                onClick={() => {
+                  setChordThatIsBeingEdited({
+                    index,
+                    value: chord,
+                  });
+                }}
+              >
+                {/* add the tooltip below for "Edit" */}
+                <AiFillEdit className="h-6 w-6" />
+              </Button>
+              {/* delete button */}
+              <Button
+                variant={"destructive"}
+                size="sm"
+                className="baseFlex h-8 w-1/2 rounded-l-none rounded-br-sm rounded-tr-none border-l-[1px]"
+                onClick={() => {
+                  const prevChords = [...chords];
+                  prevChords.splice(index, 1);
+                  setChords(prevChords);
+                }}
+              >
+                {/* add the tooltip below for "Delete" */}
+                <AiFillDelete className="h-5 w-5" />
+              </Button>
             </div>
           </div>
         ))}
         {editing && (
           <Button
             onClick={() => {
-              setCurrentlyEditingChord({
-                name: "",
-                frets: ["", "", "", "", "", ""],
+              setChordThatIsBeingEdited({
+                index: chords.length,
+                value: {
+                  name: "",
+                  frets: ["", "", "", "", "", ""],
+                },
               });
-              setShowChordModal(true);
             }}
           >
             Add chord
