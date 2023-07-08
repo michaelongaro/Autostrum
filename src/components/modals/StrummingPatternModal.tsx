e4c9a51fe1b5f6257f7be60bfc7e53462cca0a67
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-07-08 01:15:41 -0500

    init commit

---
+++ src/components/modals/StrummingPatternModal.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// since we added null as an option for chord/strumming pattern, I think maybe
+// just have a useEffect [] here that will default the strumming pattern to below
+// since don't think having it be completely empty is a good look
+//   ["", "v", ""],
+//   ["", "v", ""],
+//   ["", "^", ""],
+//   ["", "", ""],
+//   ["", "v", ""],
+//   ["", "^", ""],
+//   ["", "v", ""],
+
+function StrummingPatternModal() {
+  return <div>StrummingPatternModal</div>;
+}
+
+export default StrummingPatternModal;
