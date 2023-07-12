8724ee1c6a75e9e2e595196b47fc8f5768e126c0
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-07-12 03:38:11 -0500

    added arrow

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import * as PopoverPrimitive from "@radix-ui/react-popover";
+import { PopoverArrow } from "@radix-ui/react-popover";
 
 import { cn } from "~/lib/utils";
 
@@ -21,11 +22,14 @@ const PopoverContent = React.forwardRef<
       align={align}
       sideOffset={sideOffset}
       className={cn(
-        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "z-50 rounded-md border bg-pink-400 p-4 shadow-md outline-none animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )}
       {...props}
-    />
+    >
+      <PopoverArrow fill="rgb(253 242 248)" />
+      {props.children}
+    </PopoverPrimitive.Content>
   </PopoverPrimitive.Portal>
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
