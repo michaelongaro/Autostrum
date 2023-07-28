16ace5a4b57f8de64af5ab64530d278f3dfcf1cc
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-07-28 03:36:24 -0500

    ui overhaul + main slider + baseline logic

--- src/components/AudioControls/AudioControls.tsx
+++ src/components/AudioControls/AudioControls.tsx
@@ -12,8 +12,21 @@ import {
   SelectTrigger,
   SelectValue,
 } from "~/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 import { Slider } from "~/components/ui/slider";
+import { VerticalSlider } from "~/components/ui/verticalSlider";
+import formatSecondsToMinutes from "~/utils/formatSecondsToMinutes";
 import useSound from "~/hooks/useSound";
+import { TiArrowLoop } from "react-icons/ti";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { Button } from "~/components/ui/button";
+import { Toggle } from "~/components/ui/toggle";
+import { FaVolumeMute, FaVolumeDown, FaVolumeUp } from "react-icons/fa";
+import { RiArrowGoBackFill } from "react-icons/ri";
 
 const positionVariants = {
   expanded: {
@@ -26,13 +39,18 @@ const positionVariants = {
   },
 };
 
+const opacityAndScaleVariants = {
+  expanded: {
+    opacity: 1,
+    scale: 1,
+  },
+  closed: {
+    opacity: 0,
+    scale: 0.5,
+  },
+};
+
 function AudioControls() {
-  const [source, setSource] = useState<"Generated" | "User recorded">(
-    "Generated"
-  );
-
-  const [showVolumeSlider, setShowVolumeSlider] = useState(false);
-
   // prob create hook + place it here for useAutoscroll(),
   // it will look at the current locaiton based on metadata[currentChordIndex].location to get the
   // id (each chord div will need to have an id that matches this) and if turned on it will scroll to that
@@ -47,8 +65,19 @@ function AudioControls() {
     playbackSpeed,
     setPlaybackSpeed,
     masterVolumeGainNode,
-    playingAudio,
+    currentChordIndex,
+    setCurrentChordIndex,
+    currentlyPlayingMetadata,
+    setCurrentlyPlayingMetadata,
+    audioMetadata,
+    setAudioMetadata,
     currentInstrument,
+    tabData,
+    sectionProgression,
+    tuning,
+    bpm,
+    chords,
+    capo,
   } = useTabStore(
     (state) => ({
       recordedAudioUrl: state.recordedAudioUrl,
@@ -57,8 +86,19 @@ function AudioControls() {
       playbackSpeed: state.playbackSpeed,
       setPlaybackSpeed: state.setPlaybackSpeed,
       masterVolumeGainNode: state.masterVolumeGainNode,
-      playingAudio: state.playingAudio,
+      currentChordIndex: state.currentChordIndex,
+      setCurrentChordIndex: state.setCurrentChordIndex,
+      currentlyPlayingMetadata: state.currentlyPlayingMetadata,
+      setCurrentlyPlayingMetadata: state.setCurrentlyPlayingMetadata,
+      audioMetadata: state.audioMetadata,
+      setAudioMetadata: state.setAudioMetadata,
       currentInstrument: state.currentInstrument,
+      tabData: state.tabData,
+      sectionProgression: state.sectionProgression,
+      tuning: state.tuning,
+      bpm: state.bpm,
+      chords: state.chords,
+      capo: state.capo,
     }),
     shallow
   );
@@ -89,7 +129,7 @@ function AudioControls() {
   return (
     <motion.div
       key={"audioControls"}
-      className="baseFlex fixed z-50 w-[100vw]"
+      className="baseFlex fixed z-40 w-[100vw]"
       variants={positionVariants}
       initial="closed"
       animate="expanded"
@@ -102,7 +142,7 @@ function AudioControls() {
     >
       {/* start off with always two rows, and see how hard it would be to combine into
           one at larger widths later on */}
-      <div className="baseVertFlex h-full w-11/12 rounded-full bg-pink-600 p-2 shadow-md md:w-9/12 md:p-4 xl:w-1/2">
+      <div className="baseVertFlex h-full w-11/12 gap-2 rounded-md bg-pink-600 p-0 shadow-md md:w-9/12 md:rounded-full md:px-8 md:py-2 xl:w-1/2">
         {/* top layer: audio source, instrument, speed  + volume slider */}
         <div className="baseFlex w-full !justify-between">
           {/* audio source, instrument, speed selects*/}
@@ -110,9 +150,12 @@ function AudioControls() {
             <div className="baseFlex !flex-nowrap gap-2">
               <Label>Source</Label>
               <Select
-                value={source}
+                value={audioMetadata.type}
                 onValueChange={(value) =>
-                  setSource(value as "Generated" | "User recorded")
+                  setAudioMetadata({
+                    ...audioMetadata,
+                    type: value as "Generated" | "Artist recorded",
+                  })
                 }
               >
                 <SelectTrigger>
@@ -136,7 +179,7 @@ function AudioControls() {
             </div>
 
             <AnimatePresence mode="wait">
-              {source === "Generated" && (
+              {audioMetadata.type === "Generated" && (
                 <motion.div
                   key={"generatedSpecificAudioControls"}
                   className="baseFlex gap-2"
@@ -213,17 +256,11 @@ function AudioControls() {
                       <SelectContent>
                         <SelectGroup>
                           <SelectLabel>Speed</SelectLabel>
-
                           <SelectItem value={"0.25x"}>0.25x</SelectItem>
-
                           <SelectItem value={"0.5x"}>0.5x</SelectItem>
-
                           <SelectItem value={"0.75x"}>0.75x</SelectItem>
-
                           <SelectItem value={"1x"}>1x</SelectItem>
-
                           <SelectItem value={"1.25x"}>1.25x</SelectItem>
-
                           <SelectItem value={"1.5x"}>1.5x</SelectItem>
                         </SelectGroup>
                       </SelectContent>
@@ -234,58 +271,156 @@ function AudioControls() {
             </AnimatePresence>
           </div>
 
+          {/* most likely need to keep the break statement in there, but also need to create
+          a separate function that will idk just don't want the currentChordIndex to keep incrementing
+          while it waits for the break to be fired */}
+
+          {/* conditional "move selected section back to entire tab" */}
+          <AnimatePresence mode="wait">
+            {audioMetadata.type === "Generated" &&
+              audioMetadata.location !== null && (
+                <motion.div
+                  key={"returnToEntireTabButton"}
+                  variants={opacityAndScaleVariants}
+                  initial="closed"
+                  animate="expanded"
+                  exit="closed"
+                  transition={{ duration: 0.15 }}
+                >
+                  <Button
+                    variant="ghost" // or secondary maybe
+                    onClick={() => {
+                      void pauseTab(true);
+
+                      setAudioMetadata({
+                        ...audioMetadata,
+                        location: null,
+                      });
+                    }}
+                    className="baseFlex gap-2"
+                  >
+                    <RiArrowGoBackFill className="h-4 w-4" />
+                    <p>Return to entire tab</p>
+                  </Button>
+                </motion.div>
+              )}
+          </AnimatePresence>
+
           {/* autoscroll toggle + volume slider*/}
           <div className="baseFlex gap-2">
             {/* this should prob be either <Checkbox /> or <Toggle /> from shadcnui */}
-            <div>Autoscroll toggle</div>
+            <Toggle variant={"outline"}>Autoscroll</Toggle>
 
-            <div
-              className="relative"
-              onMouseEnter={() => setShowVolumeSlider(true)}
-              onMouseLeave={() => setShowVolumeSlider(false)}
-              onClick={() => setShowVolumeSlider(true)}
-            >
-              Volume
-              <AnimatePresence mode="wait">
-                {showVolumeSlider && (
-                  <motion.div
-                    key={"volumeSlider"}
-                    className="absolute bottom-0 right-0 h-40 w-6"
-                    variants={positionVariants} // prob just do opacity + maybe scale up from 0.5 -> 1?
-                    initial="closed"
-                    animate="expanded"
-                    exit="closed"
-                    transition={{
-                      type: "spring",
-                      bounce: 0.2,
-                      duration: 0.6,
-                    }}
-                  >
-                    <Slider
-                      orientation="vertical"
-                      value={[volume * 50]} // 100 felt too quite/narrow of a volume range
-                      min={0}
-                      max={100}
-                      step={1}
-                      onValueChange={(value) => setVolume(value[0]! / 50)} // 100 felt too quite/narrow of a volume range
-                      className="baseVertFlex h-full w-full"
-                    ></Slider>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </div>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline" className="p-2">
+                  {volume === 0 ? (
+                    <FaVolumeMute className="h-5 w-5" />
+                  ) : volume < 1 ? (
+                    <FaVolumeDown className="h-5 w-5" />
+                  ) : (
+                    <FaVolumeUp className="h-5 w-5" />
+                  )}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                className="baseVertFlex h-36 w-11 !flex-nowrap gap-2 p-2"
+                side="top"
+              >
+                <VerticalSlider
+                  value={[volume * 50]} // 100 felt too quite/narrow of a volume range
+                  min={0}
+                  max={100}
+                  step={1}
+                  onValueChange={(value) => setVolume(value[0]! / 50)} // 100 felt too quite/narrow of a volume range
+                ></VerticalSlider>
+                <p>{Math.floor(volume * 50)}%</p>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
 
         {/* bottom layer: play/pause, loop, slider*/}
-        <div className="baseFlex w-full gap-2">
+        <div className="baseFlex w-full !flex-nowrap gap-4">
           {/* audio source, instrument, speed selects*/}
 
-          <div>play/pause</div>
+          {/* play/pause button*/}
+          <Button
+            disabled={
+              currentlyPlayingMetadata === null ||
+              currentlyPlayingMetadata.length === 0 ||
+              (currentlyPlayingMetadata?.at(-1)?.elapsedSeconds ?? 0) === 0 ||
+              !currentInstrument
+            }
+            onClick={() => {
+              if (audioMetadata.playing) {
+                void pauseTab();
+              } else {
+                void playTab({
+                  tabData,
+                  rawSectionProgression: sectionProgression,
+                  tuningNotes: tuning,
+                  baselineBpm: bpm,
+                  chords,
+                  capo,
+                  location: audioMetadata.location ?? undefined,
+                });
+              }
+            }}
+            className="bg-green-600 hover:bg-green-700"
+          >
+            {audioMetadata.playing ? (
+              <BsFillPauseFill className="h-5 w-5" />
+            ) : (
+              <BsFillPlayFill className="h-5 w-5" />
+            )}
+          </Button>
 
-          <div>slider</div>
+          <div className="baseFlex w-9/12 !flex-nowrap gap-4">
+            <p>
+              {formatSecondsToMinutes(
+                currentlyPlayingMetadata?.[currentChordIndex]?.elapsedSeconds ??
+                  0
+              )}
+            </p>
 
-          <div>loop</div>
+            <Slider
+              value={[
+                currentlyPlayingMetadata?.[currentChordIndex]?.elapsedSeconds ??
+                  0,
+              ]}
+              min={0}
+              max={currentlyPlayingMetadata?.at(-1)?.elapsedSeconds ?? 0}
+              step={1}
+              disabled={
+                currentlyPlayingMetadata === null ||
+                currentlyPlayingMetadata.length === 0 ||
+                (currentlyPlayingMetadata?.at(-1)?.elapsedSeconds ?? 0) === 0 ||
+                !currentInstrument
+              }
+              onValueChange={(value) => {
+                if (currentlyPlayingMetadata) {
+                  // copilot code below, also prob for pointerDown pause audio, etc.
+
+                  const newCurrentChordIndex =
+                    currentlyPlayingMetadata.findIndex(
+                      (metadata) => metadata.elapsedSeconds === value[0]
+                    );
+
+                  setCurrentChordIndex(newCurrentChordIndex);
+                }
+              }}
+              // className=""
+            ></Slider>
+          </div>
+
+          <Toggle
+            variant={"outline"}
+            aria-label="Loop toggle"
+            className="h-8 w-8 p-1"
+          >
+            <TiArrowLoop className="h-6 w-6" />
+          </Toggle>
         </div>
       </div>
     </motion.div>
