840ae410ce2f0a6ba48a4e36b4041a48cfd5b258
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-07-28 03:39:53 -0500

    init commit

---
+++ src/components/ui/verticalSlider.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+
+import { cn } from "~/utils/utils";
+
+const VerticalSlider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex h-28 w-2 touch-none select-none items-center",
+      className
+    )}
+    orientation="vertical"
+    {...props}
+  >
+    <SliderPrimitive.Track className="relative h-full w-full grow overflow-hidden rounded-full bg-secondary">
+      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    </SliderPrimitive.Track>
+    <SliderPrimitive.Thumb className="absolute left-[-6px] block h-5 w-5 cursor-grab rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:cursor-grabbing disabled:pointer-events-none disabled:opacity-50" />
+  </SliderPrimitive.Root>
+));
+VerticalSlider.displayName = SliderPrimitive.Root.displayName;
+
+export { VerticalSlider };
---
+++ src/utils/formatSecondsToMinutes.ts
@@ -0,0 +1,12 @@
+export default function formatSecondsToMinutes(rawSeconds: number): string {
+  const minutes = Math.floor(rawSeconds / 60);
+  const seconds = rawSeconds % 60;
+
+  // This ensures the numbers are always at least two digits.
+  const formattedMinutes =
+    minutes < 10 ? "0" + `${minutes}` : minutes.toString();
+  const formattedSeconds =
+    seconds < 10 ? "0" + `${seconds}` : seconds.toString();
+
+  return formattedMinutes + ":" + formattedSeconds;
+}
