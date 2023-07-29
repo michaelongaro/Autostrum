b8c006fea6353b2e304faa0765b037dd02a20294
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-07-29 03:30:49 -0500

    preliminary logic for smooth transition css

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -6,21 +6,41 @@ import { cn } from "~/utils/utils";
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative flex w-full touch-none select-none items-center",
-      className
-    )}
-    {...props}
-  >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 cursor-grab rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:cursor-grabbing disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
-));
+>(({ className, ...props }, ref) => {
+  const [isDragging, setIsDragging] = React.useState(false);
+
+  return (
+    <SliderPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative flex w-full touch-none select-none items-center",
+        className
+      )}
+      {...props}
+    >
+      <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+        <SliderPrimitive.Range
+          style={{
+            transitionDuration: isDragging ? "0s" : "1s",
+          }}
+          className="active absolute h-full bg-primary transition-[width] ease-linear"
+        />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb
+        style={{
+          transitionDuration: isDragging ? "0s" : "1s",
+        }}
+        onPointerDown={() => setIsDragging(true)}
+        onPointerUp={() => setIsDragging(false)}
+        // also btw... this is getting applied to the actual thumb, not the span wrapper that actually
+        // needs the transition property... figure ou thow to get it on that one
+
+        // also figure out where the focus-ring is going/why we can't see it
+        className="block h-5 w-5 cursor-grab rounded-full border-2 border-primary bg-background ring-offset-background transition-all ease-linear focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:cursor-grabbing disabled:pointer-events-none disabled:opacity-50"
+      />
+    </SliderPrimitive.Root>
+  );
+});
 Slider.displayName = SliderPrimitive.Root.displayName;
 
 export { Slider };
