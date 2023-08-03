866367dee7adea1bf1cd04ce7cbee3842a7136d7
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-08-03 03:35:47 -0500

    no leading zeros + no spaces between numbers

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,11 +1,8 @@
 export default function formatDate(date: Date | number) {
   const d = new Date(date);
-  let month = ` ${d.getMonth() + 1}`;
-  let day = ` ${d.getDate()}`;
+  const month = `${d.getMonth() + 1}`;
+  const day = `${d.getDate()}`;
   const year = d.getFullYear();
 
-  if (month.length < 2) month = "0" + month;
-  if (day.length < 2) day = "0" + day;
-
-  return [month, day, year].join(" / ");
+  return [month, day, year].join("/");
 }
