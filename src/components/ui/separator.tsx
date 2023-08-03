11e906b1b0a3801821376b0a88c0a9e9cbb8e04e
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-08-03 03:36:14 -0500

    1px -> 2px width + rounded borders

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -18,8 +18,8 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        "shrink-0 rounded-full bg-border",
+        orientation === "horizontal" ? "h-[2px] w-full" : "h-full w-[2px]",
         className
       )}
       {...props}
