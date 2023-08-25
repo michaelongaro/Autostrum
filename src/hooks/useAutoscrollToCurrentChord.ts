971822e813e476aaf568f8e5859f063e7a8a62fe
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-08-24 20:25:06 -0500

    removed unused dep + comments for potential refactor

--- src/hooks/useAutoscrollToCurrentChord.ts
+++ src/hooks/useAutoscrollToCurrentChord.ts
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useTabStore } from "~/stores/TabStore";
 import { shallow } from "zustand/shallow";
-
 interface UseAutoscrollToCurrentChord {
   autoscrollEnabled: boolean;
 }
@@ -9,54 +8,81 @@ interface UseAutoscrollToCurrentChord {
 function useAutoscrollToCurrentChord({
   autoscrollEnabled,
 }: UseAutoscrollToCurrentChord) {
-  const { currentlyPlayingMetadata, audioMetadata, currentChordIndex } =
-    useTabStore(
-      (state) => ({
-        currentlyPlayingMetadata: state.currentlyPlayingMetadata,
-        audioMetadata: state.audioMetadata,
-        currentChordIndex: state.currentChordIndex,
-      }),
-      shallow
-    );
+  const { currentlyPlayingMetadata, currentChordIndex } = useTabStore(
+    (state) => ({
+      currentlyPlayingMetadata: state.currentlyPlayingMetadata,
+      currentChordIndex: state.currentChordIndex,
+    }),
+    shallow
+  );
+
+  // I am pretty sure that smooth scrolling just isn't an option due to the
+  // nature of putting instant clarity on the current chord being played over "eye candy"
+
+  // const debouncedScrollIntoView = useCallback(
+  //   debounce(() => {
+  //     const { sectionIndex, subSectionIndex, chordSequenceIndex, chordIndex } =
+  //       currentlyPlayingMetadata![currentChordIndex]!.location;
+  //     let currentElement = null;
+  //     if (chordSequenceIndex !== undefined) {
+  //       currentElement = document.getElementById(
+  //         `section${sectionIndex}-subSection${subSectionIndex}-chordSequence${chordSequenceIndex}-chord${chordIndex}`
+  //       );
+  //     } else {
+  //       currentElement = document.getElementById(
+  //         `section${sectionIndex}-subSection${subSectionIndex}-chord${chordIndex}`
+  //       );
+  //     }
+
+  //     if (currentElement) {
+  //       const rect = currentElement.getBoundingClientRect();
+  //       if (
+  //         rect.top < 0 ||
+  //         rect.bottom >
+  //           (window.innerHeight || document.documentElement.clientHeight) // not sure if second part necessary
+  //       ) {
+  //         currentElement.scrollIntoView({
+  //           behavior: "smooth",
+  //           block: "center",
+  //           inline: "center",
+  //         });
+  //       }
+  //     }
+  //   }, 250),
+  //   [currentlyPlayingMetadata, currentChordIndex]
+  // );
 
   useEffect(() => {
-    if (!currentlyPlayingMetadata) return;
+    if (!currentlyPlayingMetadata || !autoscrollEnabled) return;
 
-    if (autoscrollEnabled) {
-      const { sectionIndex, subSectionIndex, chordSequenceIndex, chordIndex } =
-        currentlyPlayingMetadata[currentChordIndex]!.location;
-      let currentElement = null;
-      if (chordSequenceIndex !== undefined) {
-        currentElement = document.getElementById(
-          `section${sectionIndex}-subSection${subSectionIndex}-chordSequence${chordSequenceIndex}-chord${chordIndex}`
-        );
-      } else {
-        currentElement = document.getElementById(
-          `section${sectionIndex}-subSection${subSectionIndex}-chord${chordIndex}`
-        );
-      }
+    const { sectionIndex, subSectionIndex, chordSequenceIndex, chordIndex } =
+      currentlyPlayingMetadata[currentChordIndex]!.location;
+    let currentElement = null;
+    if (chordSequenceIndex !== undefined) {
+      currentElement = document.getElementById(
+        `section${sectionIndex}-subSection${subSectionIndex}-chordSequence${chordSequenceIndex}-chord${chordIndex}`
+      );
+    } else {
+      currentElement = document.getElementById(
+        `section${sectionIndex}-subSection${subSectionIndex}-chord${chordIndex}`
+      );
+    }
 
-      if (currentElement) {
-        const rect = currentElement.getBoundingClientRect();
-        if (
-          rect.top < 0 ||
-          rect.bottom >
-            (window.innerHeight || document.documentElement.clientHeight) // not sure if second part necessary
-        ) {
-          currentElement.scrollIntoView({
-            behavior: "instant",
-            block: "center",
-            inline: "center",
-          });
-        }
+    if (currentElement) {
+      const rect = currentElement.getBoundingClientRect();
+      if (
+        rect.top < 0 ||
+        rect.bottom >
+          (window.innerHeight || document.documentElement.clientHeight) // not sure if second part necessary
+      ) {
+        currentElement.scrollIntoView({
+          behavior: "instant",
+          block: "center",
+          inline: "center",
+        });
       }
     }
-  }, [
-    currentlyPlayingMetadata,
-    audioMetadata,
-    currentChordIndex,
-    autoscrollEnabled,
-  ]);
+  }, [currentlyPlayingMetadata, currentChordIndex, autoscrollEnabled]);
 }
 
 export default useAutoscrollToCurrentChord;
