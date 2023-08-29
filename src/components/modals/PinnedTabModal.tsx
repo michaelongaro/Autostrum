bc32612793dde93072a19ae3a6f9b541723d9694
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-08-28 19:19:14 -0500

    init commit

---
+++ src/components/modals/PinnedTabModal.tsx
@@ -0,0 +1,136 @@
+import { useState, useEffect, type Dispatch, type SetStateAction } from "react";
+import { motion } from "framer-motion";
+import { shallow } from "zustand/shallow";
+import { api } from "~/utils/api";
+import useGetLocalStorageValues from "~/hooks/useGetLocalStorageValues";
+import useGetUrlParamFilters from "~/hooks/useGetUrlParamFilters";
+import { useClerk } from "@clerk/nextjs";
+import { useTabStore } from "~/stores/TabStore";
+import SearchInput from "../Search/SearchInput";
+import SearchResults from "../Search/SearchResults";
+import { TbPinned } from "react-icons/tb";
+import { Button } from "../ui/button";
+
+const backdropVariants = {
+  expanded: {
+    opacity: 1,
+  },
+  closed: {
+    opacity: 0,
+  },
+};
+
+interface PinnedTabModal {
+  pinnedTabIdFromDatabase: number;
+  setShowPinnedTabModal: Dispatch<SetStateAction<boolean>>;
+}
+
+function PinnedTabModal({
+  pinnedTabIdFromDatabase,
+  setShowPinnedTabModal,
+}: PinnedTabModal) {
+  const { user } = useClerk();
+  const ctx = api.useContext();
+
+  const [currentlySelectedPinnedTabId, setCurrentlySelectedPinnedTabId] =
+    useState(pinnedTabIdFromDatabase);
+
+  // const { data: artist } = api.artist.getByIdOrUsername.useQuery(
+  //   {
+  //     userId: user?.id,
+  //   },
+  //   {
+  //     enabled: !!user,
+  //   }
+  // );
+
+  const { mutate: updateArtist } = api.artist.updateArtist.useMutation({
+    onSettled: () => {
+      void ctx.artist.getByIdOrUsername.invalidate();
+      void ctx.tab.getTabById.invalidate();
+    },
+  });
+
+  // useEffect(() => {
+  //   if (
+  //     artist &&
+  //     artist.pinnedTabId !== -1 &&
+  //     currentlySelectedPinnedTabId === -1
+  //   ) {
+  //     setCurrentlySelectedPinnedTabId(artist.pinnedTabId);
+  //   }
+  // }, [artist, currentlySelectedPinnedTabId]);
+
+  useEffect(() => {
+    setCurrentlySelectedPinnedTabId(pinnedTabIdFromDatabase);
+  }, [pinnedTabIdFromDatabase]);
+
+  const { genreId, type, searchQuery, sortByRelevance, additionalSortFilter } =
+    useGetUrlParamFilters();
+
+  const viewType = useGetLocalStorageValues().viewType;
+
+  function handleUpdatePinnedTab() {
+    if (!user) return;
+
+    updateArtist({
+      id: user.id,
+      pinnedTabId: currentlySelectedPinnedTabId,
+    });
+  }
+
+  return (
+    <motion.div
+      key={"PinnedTabModalBackdrop"}
+      className="baseFlex fixed left-0 top-0 z-50 h-[100vh] w-[100vw] bg-black/50"
+      variants={backdropVariants}
+      initial="closed"
+      animate="expanded"
+      exit="closed"
+      onClick={(e) => {
+        if (e.target === e.currentTarget) {
+          // close modal
+          setShowPinnedTabModal(false);
+        }
+      }}
+    >
+      {/* prob have to be wider max for mobile */}
+      <div className="baseVertFlex w-11/12 gap-4 rounded-md bg-pink-400 p-2 shadow-sm md:p-4 lg:gap-8 xl:w-9/12">
+        {/* chord title */}
+        <div className="baseFlex gap-2">
+          <TbPinned className="h-5 w-5" />
+          <p className="text-xl font-semibold">Pinned tab</p>
+        </div>
+        <SearchInput initialSearchQueryFromUrl={searchQuery} />
+
+        <SearchResults
+          genreId={genreId}
+          type={type}
+          searchQuery={searchQuery}
+          sortByRelevance={sortByRelevance}
+          additionalSortFilter={additionalSortFilter}
+          viewType={viewType}
+          selectedPinnedTabId={currentlySelectedPinnedTabId}
+          setSelectedPinnedTabId={setCurrentlySelectedPinnedTabId}
+        />
+
+        <div className="baseFlex gap-8">
+          <Button
+            variant={"secondary"}
+            onClick={() => setShowPinnedTabModal(false)}
+          >
+            Close
+          </Button>
+          <Button
+            disabled={pinnedTabIdFromDatabase === currentlySelectedPinnedTabId}
+            onClick={handleUpdatePinnedTab}
+          >
+            Save
+          </Button>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default PinnedTabModal;
