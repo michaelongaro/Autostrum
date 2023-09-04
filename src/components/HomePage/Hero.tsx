fa7d95b9bbab05450478b522af6d71601debe691
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-09-04 18:34:23 -0500

    larger spacing so elements are more distinct

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -1,4 +1,4 @@
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import { api } from "~/utils/api";
 import { GiMusicalScore } from "react-icons/gi";
@@ -11,6 +11,8 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Separator } from "../ui/separator";
 
 function Hero() {
+  const { isSignedIn } = useAuth();
+
   const { data: fetchedTab, refetch: refetchTab } = api.tab.getTabById.useQuery(
     {
       id: 5,
@@ -22,18 +24,18 @@ function Hero() {
   return (
     <div className="baseVertFlex z-10 my-24 !flex-nowrap gap-16 xl:flex-row">
       <div className="lightGlassmorphic baseVertFlex w-5/6 items-start gap-4 rounded-xl p-4 shadow-sm sm:w-auto md:p-8">
-        <h1 className="baseVertFlex !items-start gap-2 text-4xl font-bold md:text-5xl">
+        <h1 className="baseVertFlex gap-2 text-3xl font-bold md:text-5xl">
           Welcome to
           <Image
             src="/logoWithTitle.svg"
             alt="Autostrum logo"
-            width={300}
-            height={100}
+            width={isAboveMediumViewportWidth ? 300 : 200}
+            height={isAboveMediumViewportWidth ? 100 : 75}
             priority
           />
         </h1>
 
-        <p>
+        <p className="text-center text-sm md:text-base">
           Create and share your riffs{" "}
           <span className="italic text-pink-600 underline underline-offset-2">
             exactly
@@ -41,22 +43,33 @@ function Hero() {
           how you want them to sound.
         </p>
 
-        <div className="baseFlex mt-4 gap-4">
-          <SignUpButton
-            mode="modal"
-            afterSignUpUrl="http://localhost:3000/postSignUpRegistration"
-          >
-            <Button size={"lg"}>Sign up</Button>
-          </SignUpButton>
-          <SignInButton
-            mode="modal"
-            afterSignUpUrl="http://localhost:3000/postSignUpRegistration"
-          >
-            <Button variant={"secondary"} className="h-11">
-              Sign in
-            </Button>
-          </SignInButton>
-        </div>
+        <AnimatePresence mode="wait">
+          {!isSignedIn && (
+            <motion.div
+              key={"homepageAuthContainer"}
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 1 }}
+              transition={{ duration: 0.25 }}
+              className="baseFlex mt-4 gap-4"
+            >
+              <SignUpButton
+                mode="modal"
+                afterSignUpUrl="http://localhost:3000/postSignUpRegistration"
+              >
+                <Button size={"lg"}>Sign up</Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                afterSignUpUrl="http://localhost:3000/postSignUpRegistration"
+              >
+                <Button variant={"secondary"} className="h-11">
+                  Sign in
+                </Button>
+              </SignInButton>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
 
       <div className="baseVertFlex lightGlassmorphic w-4/5 gap-4 rounded-xl p-4 shadow-sm md:max-w-[550px] md:p-8">
@@ -64,7 +77,7 @@ function Hero() {
           <div className="lightestGlassmorphic rounded-md p-2">
             <GiMusicalScore className="h-8 w-8" />
           </div>
-          <div className="baseVertFlex !items-start">
+          <div className="baseVertFlex !items-start gap-1">
             <p className="text-lg font-bold md:text-xl">Compose</p>
             <p className="text-sm md:text-base">
               Craft intricate guitar tabs with our advanced editor, complete
@@ -77,7 +90,7 @@ function Hero() {
           <div className="lightestGlassmorphic rounded-md p-2">
             <HiOutlineLightBulb className="h-8 w-8" />
           </div>
-          <div className="baseVertFlex !items-start">
+          <div className="baseVertFlex !items-start gap-1">
             <p className="text-lg font-bold md:text-xl">Find inspiration</p>
             <p className="text-sm md:text-base">
               Explore an ever growing library of tabs and discover new talents
@@ -90,7 +103,7 @@ function Hero() {
           <div className="lightestGlassmorphic rounded-md p-2">
             <BsBarChartLine className="h-8 w-8" />
           </div>
-          <div className="baseVertFlex !items-start">
+          <div className="baseVertFlex !items-start gap-1">
             <p className="text-lg font-bold md:text-xl">Practice</p>
             <p className="text-sm md:text-base">
               Hear your tabs come to life with true-to-life audio playback,
@@ -99,57 +112,58 @@ function Hero() {
           </div>
         </div>
 
-        {/* needs to have placeholder + animate presence shii */}
-        <AnimatePresence mode="wait">
-          {fetchedTab ? (
-            <GridTabCard
-              tab={fetchedTab}
-              refetchTab={refetchTab}
-              width={isAboveMediumViewportWidth ? 396.25 : undefined}
-              height={isAboveMediumViewportWidth ? 180 : undefined}
-            />
-          ) : (
-            <motion.div
-              key={"homepageTabPlaceholder"}
-              initial={{ opacity: 0, scale: 0.9 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 1 }}
-              transition={{ duration: 0.25 }}
-              className="lightestGlassmorphic baseVertFlex rounded-md border-2"
-            >
-              <div
-                style={{
-                  width: isAboveMediumViewportWidth ? 396.25 : 150,
-                  height: isAboveMediumViewportWidth ? 180 : 100,
-                }}
-                className="animate-pulse rounded-t-sm bg-pink-300"
-              ></div>
+        <div className="baseFlex mt-4 w-full">
+          <AnimatePresence mode="wait">
+            {fetchedTab ? (
+              <GridTabCard
+                tab={fetchedTab}
+                refetchTab={refetchTab}
+                width={isAboveMediumViewportWidth ? 396.25 : undefined}
+                height={isAboveMediumViewportWidth ? 180 : undefined}
+              />
+            ) : (
+              <motion.div
+                key={"homepageTabPlaceholder"}
+                initial={{ opacity: 0, scale: 0.9 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 1 }}
+                transition={{ duration: 0.25 }}
+                className="lightestGlassmorphic baseVertFlex rounded-md border-2"
+              >
+                <div
+                  style={{
+                    width: isAboveMediumViewportWidth ? 396.25 : 150,
+                    height: isAboveMediumViewportWidth ? 180 : 100,
+                  }}
+                  className="animate-pulse rounded-t-sm bg-pink-300"
+                ></div>
 
-              <Separator />
+                <Separator />
 
-              <div className="baseFlex w-full !items-end !justify-between">
-                {/* title, date, and genre */}
-                <div className="baseVertFlex mt-2 !items-start gap-2 pb-2 pl-2">
-                  <div className="h-8 w-48 animate-pulse rounded-md bg-pink-300"></div>
-                  <div className="h-6 w-16 animate-pulse rounded-md bg-pink-300"></div>
-                  <div className="h-4 w-8 animate-pulse rounded-md bg-pink-300"></div>
-                </div>
+                <div className="baseFlex w-full !items-end !justify-between">
+                  {/* title, date, and genre */}
+                  <div className="baseVertFlex mt-2 !items-start gap-2 pb-2 pl-2">
+                    <div className="h-8 w-48 animate-pulse rounded-md bg-pink-300"></div>
+                    <div className="h-6 w-16 animate-pulse rounded-md bg-pink-300"></div>
+                    <div className="h-4 w-8 animate-pulse rounded-md bg-pink-300"></div>
+                  </div>
 
-                {/* artist link & likes & play button */}
-                <div className="baseVertFlex gap-2">
-                  <div className="baseFlex w-full !flex-nowrap !justify-evenly rounded-tl-md border-l-2 border-t-2">
-                    {/* likes button */}
-                    <div className="h-8 w-16 animate-pulse rounded-r-none rounded-bl-none rounded-tl-sm  bg-pink-300"></div>
-                    <Separator className="h-8 w-[1px]" />
+                  {/* artist link & likes & play button */}
+                  <div className="baseVertFlex gap-2">
+                    <div className="baseFlex w-full !flex-nowrap !justify-evenly rounded-tl-md border-l-2 border-t-2">
+                      {/* likes button */}
+                      <div className="h-8 w-16 animate-pulse rounded-r-none rounded-bl-none rounded-tl-sm  bg-pink-300"></div>
+                      <Separator className="h-8 w-[1px]" />
 
-                    {/* play/pause button*/}
-                    <div className="h-8 w-16 animate-pulse rounded-l-none rounded-br-sm rounded-tr-none  bg-pink-300"></div>
+                      {/* play/pause button*/}
+                      <div className="h-8 w-16 animate-pulse rounded-l-none rounded-br-sm rounded-tr-none  bg-pink-300"></div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
