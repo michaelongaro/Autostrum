d27f9fb6f1300d0b1eca6eb16dc07403394d4d25
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-09-06 19:29:51 -0500

    removed comment that will not be used anymore

--- src/hooks/useAutoscrollToCurrentChord.ts
+++ src/hooks/useAutoscrollToCurrentChord.ts
@@ -18,42 +18,6 @@ function useAutoscrollToCurrentChord({
       shallow
     );
 
-  // I am pretty sure that smooth scrolling just isn't an option due to the
-  // nature of putting instant clarity on the current chord being played over "eye candy"
-
-  // const debouncedScrollIntoView = useCallback(
-  //   debounce(() => {
-  //     const { sectionIndex, subSectionIndex, chordSequenceIndex, chordIndex } =
-  //       currentlyPlayingMetadata![currentChordIndex]!.location;
-  //     let currentElement = null;
-  //     if (chordSequenceIndex !== undefined) {
-  //       currentElement = document.getElementById(
-  //         `section${sectionIndex}-subSection${subSectionIndex}-chordSequence${chordSequenceIndex}-chord${chordIndex}`
-  //       );
-  //     } else {
-  //       currentElement = document.getElementById(
-  //         `section${sectionIndex}-subSection${subSectionIndex}-chord${chordIndex}`
-  //       );
-  //     }
-
-  //     if (currentElement) {
-  //       const rect = currentElement.getBoundingClientRect();
-  //       if (
-  //         rect.top < 0 ||
-  //         rect.bottom >
-  //           (window.innerHeight || document.documentElement.clientHeight) // not sure if second part necessary
-  //       ) {
-  //         currentElement.scrollIntoView({
-  //           behavior: "smooth",
-  //           block: "center",
-  //           inline: "center",
-  //         });
-  //       }
-  //     }
-  //   }, 250),
-  //   [currentlyPlayingMetadata, currentChordIndex]
-  // );
-
   useEffect(() => {
     if (
       !audioMetadata.playing ||
@@ -65,6 +29,7 @@ function useAutoscrollToCurrentChord({
     const { sectionIndex, subSectionIndex, chordSequenceIndex, chordIndex } =
       currentlyPlayingMetadata[currentChordIndex]!.location;
     let currentElement = null;
+
     if (chordSequenceIndex !== undefined) {
       currentElement = document.getElementById(
         `section${sectionIndex}-subSection${subSectionIndex}-chordSequence${chordSequenceIndex}-chord${chordIndex}`
