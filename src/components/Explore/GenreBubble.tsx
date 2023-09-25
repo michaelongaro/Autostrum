03424524e4040865f8a28308a9fa04c0bf01629a
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-09-25 04:05:02 -0500

    gap-1 on mobile, gap-2 on desktop

--- src/components/Explore/GenreBubble.tsx
+++ src/components/Explore/GenreBubble.tsx
@@ -117,7 +117,7 @@ function GenreBubble(genre: GenreWithTotalTabNumbers) {
       style={{
         backgroundColor: genre.color,
       }}
-      className={`baseVertFlex group relative h-36 w-full cursor-pointer !items-start !justify-start gap-2 rounded-lg p-4 shadow-md transition-all hover:shadow-lg sm:!justify-center sm:p-6 ${
+      className={`baseVertFlex group relative h-36 w-full cursor-pointer !items-start !justify-start gap-1 rounded-lg p-4 shadow-md transition-all hover:shadow-lg sm:!justify-center sm:p-6 md:gap-2 ${
         mouseDownOnGenre ? "brightness-75" : "brightness-100"
       }`}
       onMouseEnter={() => setHoveringOnGenre(true)}
