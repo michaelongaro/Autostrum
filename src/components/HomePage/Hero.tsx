2c6380231f2b3dbd1e096d7199c8e52b1cb4d475
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-10-07 04:32:50 -0500

    slight style tweaks

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -22,9 +22,10 @@ function Hero({
   );
 
   const isAboveMediumViewportWidth = useViewportWidthBreakpoint(768);
+  const isAboveExtraLargeViewportWidth = useViewportWidthBreakpoint(1280);
 
   return (
-    <div className="baseVertFlex z-10 my-24 !flex-nowrap gap-16">
+    <div className="baseVertFlex z-10 my-24 !flex-nowrap gap-16 md:gap-24">
       <div className="lightGlassmorphic baseVertFlex w-5/6 items-start gap-2 rounded-xl p-4 shadow-sm sm:w-auto md:gap-4 md:p-8">
         <div className="baseVertFlex gap-4">
           <h1 className="baseVertFlex gap-2 text-3xl font-bold md:text-5xl">
@@ -71,46 +72,101 @@ function Hero({
         )}
       </div>
 
-      <div className="baseVertFlex lightGlassmorphic w-4/5 !flex-nowrap gap-4 rounded-xl p-4 shadow-sm md:max-w-[550px] md:p-8 xl:w-[950px] xl:max-w-[950px]">
-        <div className="baseVertFlex !flex-nowrap gap-4 xl:flex-row xl:gap-12">
-          <div className="baseFlex !flex-nowrap gap-4 xl:flex-col xl:!items-start">
-            <div className="lightestGlassmorphic rounded-md p-2">
-              <GiMusicalScore className="h-8 w-8" />
+      <div className="baseVertFlex lightGlassmorphic w-4/5 !flex-nowrap gap-8 rounded-xl p-4 shadow-sm md:max-w-[550px] md:gap-4 md:p-8 xl:w-[950px] xl:max-w-[950px]">
+        <div className="baseVertFlex !flex-nowrap gap-8 xl:flex-row xl:gap-12">
+          {/* ideally would try to be smarter about mobile vs desktop styles/org rather than repeat
+              myself twice here */}
+          {isAboveExtraLargeViewportWidth ? (
+            <div className="baseFlex !flex-nowrap gap-3 xl:flex-col xl:!items-start">
+              <div className="baseFlex w-full !justify-start gap-2">
+                <div className="lightestGlassmorphic mr-2 rounded-md p-2">
+                  <GiMusicalScore className="h-8 w-8" />
+                </div>
+                <p className="text-lg font-bold md:text-xl">Compose</p>
+              </div>
+              <div className="baseVertFlex !items-start gap-1">
+                <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
+                  Craft intricate guitar tabs with our advanced editor, complete
+                  with strumming patterns, keyboard navigation, and more!
+                </p>
+              </div>
             </div>
-            <div className="baseVertFlex !items-start gap-1">
-              <p className="text-lg font-bold md:text-xl">Compose</p>
-              <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
-                Craft intricate guitar tabs with our advanced editor, complete
-                with strumming patterns, keyboard navigation, and more!
-              </p>
+          ) : (
+            <div className="baseFlex !flex-nowrap gap-4 xl:flex-col xl:!items-start">
+              <div className="lightestGlassmorphic rounded-md p-2">
+                <GiMusicalScore className="h-8 w-8" />
+              </div>
+              <div className="baseVertFlex !items-start gap-1">
+                <p className="text-lg font-bold md:text-xl">Compose</p>
+                <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
+                  Craft intricate guitar tabs with our advanced editor, complete
+                  with strumming patterns, keyboard navigation, and more!
+                </p>
+              </div>
             </div>
-          </div>
+          )}
 
-          <div className="baseFlex !flex-nowrap gap-4 xl:flex-col xl:!items-start">
-            <div className="lightestGlassmorphic rounded-md p-2">
-              <HiOutlineLightBulb className="h-8 w-8" />
+          {isAboveExtraLargeViewportWidth ? (
+            <div className="baseFlex !flex-nowrap gap-3 xl:flex-col xl:!items-start">
+              <div className="baseFlex w-full !justify-start gap-2">
+                <div className="lightestGlassmorphic mr-2 rounded-md p-2">
+                  <HiOutlineLightBulb className="h-8 w-8" />
+                </div>
+                <p className="text-lg font-bold md:text-xl">Find inspiration</p>
+              </div>
+              <div className="baseVertFlex !items-start gap-1">
+                <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
+                  Explore an ever growing library of tabs and discover new
+                  talents in our weekly featured artist section.
+                </p>
+              </div>
             </div>
-            <div className="baseVertFlex !items-start gap-1">
-              <p className="text-lg font-bold md:text-xl">Find inspiration</p>
-              <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
-                Explore an ever growing library of tabs and discover new talents
-                in our weekly featured artist section.
-              </p>
+          ) : (
+            <div className="baseFlex !flex-nowrap gap-4 xl:flex-col xl:!items-start">
+              <div className="lightestGlassmorphic rounded-md p-2">
+                <HiOutlineLightBulb className="h-8 w-8" />
+              </div>
+              <div className="baseVertFlex !items-start gap-1">
+                <p className="text-lg font-bold md:text-xl">Find inspiration</p>
+                <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
+                  Explore an ever growing library of tabs and discover new
+                  talents in our weekly featured artist section.
+                </p>
+              </div>
             </div>
-          </div>
+          )}
 
-          <div className="baseFlex !flex-nowrap gap-4 xl:flex-col xl:!items-start">
-            <div className="lightestGlassmorphic rounded-md p-2">
-              <BsBarChartLine className="h-8 w-8" />
+          {isAboveExtraLargeViewportWidth ? (
+            <div className="baseFlex !flex-nowrap gap-3 xl:flex-col xl:!items-start">
+              <div className="baseFlex w-full !justify-start gap-2">
+                <div className="lightestGlassmorphic mr-2 rounded-md p-2">
+                  <BsBarChartLine className="h-8 w-8" />
+                </div>
+                <p className="text-lg font-bold md:text-xl">Practice</p>
+              </div>
+              <div className="baseVertFlex !items-start gap-1">
+                <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
+                  Play along with any tab, varying the playback speed,
+                  instrument, or directly with the artist&apos;s official
+                  recording.
+                </p>
+              </div>
             </div>
-            <div className="baseVertFlex !items-start gap-1">
-              <p className="text-lg font-bold md:text-xl">Practice</p>
-              <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
-                Play along with any tab, varying the playback speed, instrument,
-                or directly with the artist&apos;s official recording.
-              </p>
+          ) : (
+            <div className="baseFlex !flex-nowrap gap-4 xl:flex-col xl:!items-start">
+              <div className="lightestGlassmorphic rounded-md p-2">
+                <BsBarChartLine className="h-8 w-8" />
+              </div>
+              <div className="baseVertFlex !items-start gap-1">
+                <p className="text-lg font-bold md:text-xl">Practice</p>
+                <p className="text-sm md:text-base xl:h-[125px] xl:w-[250px]">
+                  Play along with any tab, varying the playback speed,
+                  instrument, or directly with the artist&apos;s official
+                  recording.
+                </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
 
         <div className="baseFlex mt-4 w-full">
