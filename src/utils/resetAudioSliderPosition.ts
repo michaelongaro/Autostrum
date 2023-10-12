9975cb639e428d4f63e470caf528fb1c2ad1a75b
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-10-11 21:14:01 -0500

    hack to fix radix/slider's initial thumb position

--- src/utils/resetAudioSliderPosition.ts
+++ src/utils/resetAudioSliderPosition.ts
@@ -1,4 +1,6 @@
-export default function resetAudioSliderPosition() {
+export default function resetAudioSliderPosition(
+  correctInitialRenderPosition?: boolean
+) {
   // semi-hacky way to *instantly* reset thumb + track position to
   // beginning of slider w/ no transition
   const audioSliderNode = document.getElementById("audioSlider");
@@ -11,6 +13,8 @@ export default function resetAudioSliderPosition() {
     childElements[0]!.children[0]!.style.right = "100%";
 
     childElements[1]!.style.transition = "none";
-    childElements[1]!.style.left = "0";
+    childElements[1]!.style.left = correctInitialRenderPosition
+      ? "calc(0% + 10px)"
+      : "0";
   }
 }
