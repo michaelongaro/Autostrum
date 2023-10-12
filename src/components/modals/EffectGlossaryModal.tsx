efd430461d87972172203af7d122cd9a80f8b898
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-10-11 19:38:10 -0500

    disables scroll while active (mobile addressbar issues)

--- src/components/modals/EffectGlossaryModal.tsx
+++ src/components/modals/EffectGlossaryModal.tsx
@@ -1,4 +1,8 @@
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { shallow } from "zustand/shallow";
+import { useTabStore } from "~/stores/TabStore";
+
 import EffectGlossary from "../ui/EffectGlossary";
 
 const backdropVariants = {
@@ -11,6 +15,36 @@ const backdropVariants = {
 };
 
 function EffectGlossaryModal() {
+  const { setPreventFramerLayoutShift } = useTabStore(
+    (state) => ({
+      setPreventFramerLayoutShift: state.setPreventFramerLayoutShift,
+    }),
+    shallow
+  );
+
+  useEffect(() => {
+    setPreventFramerLayoutShift(true);
+
+    setTimeout(() => {
+      const offsetY = window.scrollY;
+      document.body.style.top = `${-offsetY}px`;
+      document.body.classList.add("noScroll");
+    }, 50);
+
+    return () => {
+      setPreventFramerLayoutShift(false);
+
+      setTimeout(() => {
+        const offsetY = Math.abs(
+          parseInt(`${document.body.style.top || 0}`, 10)
+        );
+        document.body.classList.remove("noScroll");
+        document.body.style.removeProperty("top");
+        window.scrollTo(0, offsetY || 0);
+      }, 50);
+    };
+  }, [setPreventFramerLayoutShift]);
+
   return (
     <motion.div
       key={"EffectGlossaryModalBackdrop"}
