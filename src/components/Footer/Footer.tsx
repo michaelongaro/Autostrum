24c4e62a4da06cc486be4bf3f015340aa0a27487
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-11-07 18:58:52 -0600

    better box-shadows

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { Separator } from "~/components/ui/separator";
 
 function Footer() {
   return (
-    <footer className="footerBackgroundGradient baseFlex z-30 h-16 w-full gap-4 shadow-sm">
+    <footer className="footerBackgroundGradient baseFlex z-30 h-16 w-full gap-4">
       <Button variant={"ghost"} asChild>
         <a
           href="mailto:michael.ongaro.dev@gmail.com"
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import MobileHeader from "./MobileHeader";
 
 function Header() {
   return (
-    <nav className="headerBackgroundGradient sticky left-0 top-0 z-[49] grid h-16 w-full grid-cols-1 grid-rows-1 shadow-sm">
+    <nav className="headerBackgroundGradient sticky left-0 top-0 z-[49] grid h-16 w-full grid-cols-1 grid-rows-1 shadow-lightGlassmorphic">
       <DesktopHeader />
       <MobileHeader />
     </nav>
