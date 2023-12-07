3eb9b7e5169114368b36bc23b70fabdcf7b0173c
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-12-06 21:52:40 -0600

    first pass at "pre-scrolling" to next chord

--- src/hooks/useAutoscrollToCurrentChord.ts
+++ src/hooks/useAutoscrollToCurrentChord.ts
@@ -22,11 +22,13 @@ function useAutoscrollToCurrentChord({
     currentlyPlayingMetadata,
     currentChordIndex,
     audioMetadata,
+    playbackSpeed,
   } = useTabStore((state) => ({
     editing: state.editing,
     currentlyPlayingMetadata: state.currentlyPlayingMetadata,
     currentChordIndex: state.currentChordIndex,
     audioMetadata: state.audioMetadata,
+    playbackSpeed: state.playbackSpeed,
   }));
 
   useEffect(() => {
@@ -35,13 +37,22 @@ function useAutoscrollToCurrentChord({
       (previousChordYScrollValue === -1 && currentChordIndex === 0) ||
       (editing && !audioMetadata.playing) ||
       !currentlyPlayingMetadata ||
-      !autoscrollEnabled
+      !autoscrollEnabled ||
+      currentChordIndex === currentlyPlayingMetadata.length - 1 // always are going to be scrolling to the next chord (for better ux)
     )
       return;
 
+    const durationOfCurrentChord =
+      60 /
+      ((currentlyPlayingMetadata[currentChordIndex]!.bpm /
+        Number(
+          currentlyPlayingMetadata[currentChordIndex]!.noteLengthMultiplier
+        )) *
+        playbackSpeed);
+
     const { sectionIndex, subSectionIndex, chordSequenceIndex, chordIndex } =
-      currentlyPlayingMetadata[currentChordIndex]!.location;
-    let currentElement = null;
+      currentlyPlayingMetadata[currentChordIndex + 1]!.location;
+    let currentElement: HTMLElement | null = null;
 
     if (chordSequenceIndex !== undefined) {
       currentElement = document.getElementById(
@@ -53,51 +64,60 @@ function useAutoscrollToCurrentChord({
       );
     }
 
-    if (currentElement) {
-      const rect = currentElement.getBoundingClientRect();
-      const currentChordYScrollValue = rect.y;
+    if (!currentElement) return;
 
-      if (
-        previousChordYScrollValue !== -1 &&
-        Math.abs(previousChordYScrollValue - currentChordYScrollValue) < 50 &&
-        previousChordSectionIndex === sectionIndex
-      ) {
-        return;
-      }
+    const rect = currentElement.getBoundingClientRect();
+    const currentChordYScrollValue = rect.y;
 
-      const isAboveLargeViewport = window.innerWidth >= 1024;
-      const targetIsWayOutOfViewport =
-        Math.abs(previousChordYScrollValue - currentChordYScrollValue) >
-        window.innerHeight * 3;
-      const targetIsOutOfViewportWithMargins =
-        rect.top < 100 ||
-        rect.bottom > window.innerHeight - (isAboveLargeViewport ? 120 : 100);
-      const isFirstChordOfNewStrummingSection =
-        chordSequenceIndex !== undefined &&
-        previousChordSectionIndex !== sectionIndex &&
-        chordIndex === 0;
-
-      if (
-        targetIsOutOfViewportWithMargins ||
-        isFirstChordOfNewStrummingSection
-      ) {
-        scroller.scrollTo(currentElement.id, {
-          duration: targetIsWayOutOfViewport ? 0 : 200, // prevents jarring scroll
-          delay: 0,
-          smooth: "easeInOutQuad",
-          offset: isFirstChordOfNewStrummingSection
-            ? -160
-            : -(
-                window.innerHeight / 2 -
-                rect.height / 2 -
-                window.innerHeight * 0.26
-              ),
-        });
-      }
-
-      setPreviousChordYScrollValue(currentChordYScrollValue);
-      setPreviousChordSectionIndex(sectionIndex);
+    if (
+      previousChordYScrollValue !== -1 &&
+      Math.abs(previousChordYScrollValue - currentChordYScrollValue) < 50 &&
+      previousChordSectionIndex === sectionIndex
+    ) {
+      return;
     }
+
+    const isAboveLargeViewport = window.innerWidth >= 1024;
+    const targetIsWayOutOfViewport =
+      Math.abs(previousChordYScrollValue - currentChordYScrollValue) >
+      window.innerHeight * 3;
+    const targetIsOutOfViewportWithMargins =
+      rect.top < 100 ||
+      rect.bottom > window.innerHeight - (isAboveLargeViewport ? 120 : 100);
+    const isFirstChordOfNewStrummingSection =
+      chordSequenceIndex !== undefined &&
+      previousChordSectionIndex !== sectionIndex &&
+      chordIndex === 0;
+
+    setTimeout(
+      () => {
+        if (
+          currentElement &&
+          (targetIsOutOfViewportWithMargins ||
+            isFirstChordOfNewStrummingSection)
+        ) {
+          scroller.scrollTo(currentElement.id, {
+            duration: targetIsWayOutOfViewport ? 0 : 200, // prevents jarring scroll
+            delay: 0,
+            smooth: "easeInOutQuad",
+            offset: isFirstChordOfNewStrummingSection
+              ? -160
+              : -(
+                  window.innerHeight / 2 -
+                  rect.height / 2 -
+                  window.innerHeight * 0.26
+                ),
+          });
+        }
+
+        setPreviousChordYScrollValue(currentChordYScrollValue);
+        setPreviousChordSectionIndex(sectionIndex);
+      },
+      // TODO: technically I think you would want to abstract this whole function out to optionally do the
+      // current chord or the next chord based on whether or not the audio is playing, since when scrolling
+      // backwards through the tab I think it is off by one.
+      audioMetadata.playing ? durationOfCurrentChord * 0.85 : 0
+    );
   }, [
     editing,
     currentlyPlayingMetadata,
@@ -106,6 +126,7 @@ function useAutoscrollToCurrentChord({
     audioMetadata,
     previousChordYScrollValue,
     previousChordSectionIndex,
+    playbackSpeed,
   ]);
 }
 
