50cb3cf5c2e66564112d7c0467d2376fe7b0d773
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-12-11 18:45:33 -0600

    slightly darker primaryButton boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,7 +56,7 @@ module.exports = {
         },
       },
       boxShadow: {
-        primaryButton: "0 1px 2px 0 rgb(0 0 0 / 0.2)",
+        primaryButton: "0 1px 2px 0 rgb(0 0 0 / 0.25)",
         lightGlassmorphic: "0 2px 3px 0 rgb(0 0 0 / 0.15)",
         lighterGlassmorphic: "0 2px 3px 0 rgb(0 0 0 / 0.18)",
         lightestGlassmorphic: "0px 2px 3px 0 rgb(0 0 0 / 0.23)",
