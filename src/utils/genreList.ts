01890658d0eb2c0cf2032f976ecdc5e5778831c6
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-05-21 18:22:01 -0500

    using tailwind 700 level colors instead of radix ones for better contrast

--- src/utils/genreList.ts
+++ src/utils/genreList.ts
@@ -1,16 +1,16 @@
 // FYI: I recognize that this no longer is a fantastic variable name...
 
 export const genreList = new Map<string, string>([
-  ["Rock", "#B54548"],
-  ["Indie", "#A35829"],
-  ["Jazz", "#836A21"],
-  ["Pop", "#3E7949"],
-  ["Country", "#207E73"],
-  ["Folk", "#197CAE"],
-  ["Blues", "#2870BD"],
-  ["Hip-Hop", "#8457AA"],
-  ["Electronic", "#A84885"],
-  ["Classical", "#7C5F46"],
-  ["Metal", "#5A6169"],
-  ["Misc.", "#2F7C57"],
+  ["Rock", "oklch(50.5% 0.213 27.518)"],
+  ["Indie", "oklch(55.3% 0.195 38.402)"],
+  ["Jazz", "oklch(68.1% 0.162 75.834)"],
+  ["Pop", "oklch(64.8% 0.2 131.684)"],
+  ["Folk", "oklch(52% 0.105 223.128)"],
+  ["Country", "oklch(51.1% 0.096 186.391)"],
+  ["Blues", "oklch(48.8% 0.243 264.376)"],
+  ["Hip-Hop", "oklch(49.6% 0.265 301.924)"],
+  ["Electronic", "oklch(52.5% 0.223 3.958)"],
+  ["Classical", "oklch(55.5% 0.163 48.998)"],
+  ["Metal", "oklch(37% 0.013 285.805)"],
+  ["Misc.", "oklch(50% 0.134 242.749)"],
 ]);
