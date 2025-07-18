1d3cd8e311450b43e26a434346c3b00fca38dead
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-07-18 02:15:14 -0500

    adjusted note length modifier hotkeys to not overlap with existing hotkeys

--- src/components/Dialogs/TipsDialog.tsx
+++ src/components/Dialogs/TipsDialog.tsx
@@ -102,8 +102,8 @@ function TipsDialog({ showTipsDialog, setShowTipsDialog }: TipsDialog) {
               <div className="mt-3 h-[1px] w-3 shrink-0 bg-foreground" />
               <span>
                 Enter
-                <kbd className="ml-1.5">t</kbd> / <kbd>y</kbd> /{" "}
-                <kbd className="mr-1.5">u</kbd>
+                <kbd className="ml-1.5">j</kbd> / <kbd>k</kbd> /{" "}
+                <kbd className="mr-1.5">l</kbd>
                 to change the current chord&apos;s note length between 1/4th,
                 1/8th, and 1/16th respectively.
               </span>
