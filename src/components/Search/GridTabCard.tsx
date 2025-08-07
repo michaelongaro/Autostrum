4856501d5cb69a174eddffc675c4390c53d9a67e
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-08-06 22:41:39 -0500

    increased z-index of edit button to prevent unwanted tab screenshot darkening

--- src/components/Search/GridTabCard.tsx
+++ src/components/Search/GridTabCard.tsx
@@ -250,7 +250,7 @@ function GridTabCard({
                 <Link
                   prefetch={false}
                   href={`/tab/${minimalTab.id}/edit`}
-                  className="absolute right-14 top-2 !size-10 !p-0"
+                  className="absolute right-14 top-2 z-10 !size-10 !p-0"
                 >
                   <MdModeEditOutline className="size-5" />
                 </Link>
