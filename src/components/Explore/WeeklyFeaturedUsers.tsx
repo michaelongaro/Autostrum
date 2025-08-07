ad122851d0ab48d53aed936577d15aefb7262076
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-08-06 19:46:21 -0500

    slightly wider mobile cards

--- src/components/Explore/WeeklyFeaturedUsers.tsx
+++ src/components/Explore/WeeklyFeaturedUsers.tsx
@@ -390,7 +390,7 @@ function WeeklyFeaturedUsers({
           {weeklyFeaturedUsers.map((user, index) => (
             <CarouselItem
               key={user.userId}
-              className="baseFlex basis-[90%] pl-12 sm:basis-[70%] lg:h-[280px] lg:basis-[711px]"
+              className="baseFlex basis-[95%] pl-12 sm:basis-[70%] lg:h-[280px] lg:basis-[711px]"
             >
               <div className="baseFlex size-full rounded-lg border bg-secondary-active/50 px-4 py-4 shadow-sm">
                 <div
