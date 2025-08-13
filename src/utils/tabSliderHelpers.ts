7e440361e3b603098df7bf95de091034fced8fa8
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-08-12 19:38:36 -0500

    requestAnimationFrame didn't work, reverting to forced reflow for consistency

--- src/utils/tabSliderHelpers.ts
+++ src/utils/tabSliderHelpers.ts
@@ -1,60 +1,28 @@
-function resetTabSliderPosition() {
-  // semi-hacky way to *instantly* reset thumb + track position to
-  // beginning of slider w/ no transition
-  const audioSliderNode = document.getElementById("audioSlider");
-  if (audioSliderNode) {
-    const childElements = Array.from(
-      audioSliderNode.children,
-    ) as HTMLSpanElement[];
-    childElements[0]!.children[0]!.style.transition = "none";
-    childElements[0]!.children[0]!.style.right = "100%";
-    childElements[1]!.style.transition = "none";
-    // calc prob not necessary, but consistent w/ radix's styles
-    childElements[1]!.style.left = "calc(0% + 10px)";
-  }
-}
+function resetProgressTabSliderPosition(type: "editing" | "playback") {
+  const track = document.getElementById(
+    type === "editing" ? "editingSliderTrack" : "playbackSliderTrack",
+  );
+  const thumb = document.getElementById(
+    type === "editing" ? "editingSliderThumb" : "playbackSliderThumb",
+  );
 
-function returnTransitionToTabSlider() {
-  // used in effects in <AudioControls /> when the slider position state
-  // is actually updated and it is safe to give the slider it's linear
-  // transition back.
-  const audioSliderNode = document.getElementById("audioSlider");
-  if (audioSliderNode) {
-    const childElements = Array.from(
-      audioSliderNode.children,
-    ) as HTMLSpanElement[];
+  if (!track || !thumb) return;
 
-    childElements[0]!.children[0]!.style.transition = "right 1s linear";
-    childElements[1]!.style.transition = "left 1s linear";
-  }
-}
+  const prevTransitionDuration = thumb.style.transitionDuration;
 
-function resetPlaybackTabSliderPosition() {
-  const playbackSliderTrack = document.getElementById("playbackSliderTrack");
-  const playbackSliderThumb = document.getElementById("playbackSliderThumb");
+  track.style.transitionDuration = "0s";
+  track.style.transform = "scaleX(0)";
 
-  if (!playbackSliderTrack || !playbackSliderThumb) return;
+  thumb.style.transitionDuration = "0s";
+  thumb.style.transform = "translate(-9px, -5px)"; // default px values for "starting" thumb position
 
-  const prevPlaybackTransitionDuration =
-    playbackSliderThumb.style.transitionDuration;
-
-  playbackSliderTrack.style.transitionDuration = "0s";
-  playbackSliderTrack.style.transform = "scaleX(0)";
-
-  playbackSliderThumb.style.transitionDuration = "0s";
-  playbackSliderThumb.style.transform = "translate(-9px, -5px)"; // default px values for "starting" thumb position
+  // force reflow to apply the new styles
+  void thumb.offsetWidth;
+  void track.offsetWidth;
 
   // re-apply the transition on the next frame after resetting the position
-  requestAnimationFrame(() => {
-    playbackSliderTrack.style.transitionDuration =
-      prevPlaybackTransitionDuration;
-    playbackSliderThumb.style.transitionDuration =
-      prevPlaybackTransitionDuration;
-  });
+  track.style.transitionDuration = prevTransitionDuration;
+  thumb.style.transitionDuration = prevTransitionDuration;
 }
 
-export {
-  resetTabSliderPosition,
-  returnTransitionToTabSlider,
-  resetPlaybackTabSliderPosition,
-};
+export { resetProgressTabSliderPosition };
