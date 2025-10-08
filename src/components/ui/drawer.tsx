c16bdca7a35ed202efe088023a0dab0e3cde5a7b
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-10-08 16:29:40 -0500

    lowered max-w from xl to md

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -50,7 +50,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto w-full max-w-xl flex-col place-self-center rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto w-full max-w-md flex-col place-self-center rounded-t-[10px] border bg-background",
         className,
       )}
       {...props}
