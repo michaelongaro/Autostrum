12b773b0296e472236350d48717fd1aaf77cc412
Michael Ongaro <michael.ongaro.dev@gmail.com>
2026-05-22 23:52:00 -0500

    backdrop-blur-sm took away too much visibility from main page content

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -33,7 +33,7 @@ const DrawerOverlay = React.forwardRef<
       animationTimingFunction: "ease-in-out",
     }}
     className={cn(
-      "fixed inset-0 z-50 bg-black/40 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className,
     )}
     {...props}
