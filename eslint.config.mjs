6230e3335ede70c9b898913c45a3e9da3a600ed8
Michael Ongaro <michael.ongaro.dev@gmail.com>
2026-07-20 16:12:29 -0500

    trying to consolidate/fix my eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   ...nextCoreWebVitals,
-  {
-    ignores: [".next/**"],
-  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
   {
     files: ["**/*.ts", "**/*.tsx"],
     extends: [
