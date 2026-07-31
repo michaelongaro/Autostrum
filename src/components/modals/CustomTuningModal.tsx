5fa130d1aedf9b2e240ce97e0b0c5a0e881353d5
Michael Ongaro <michael.ongaro.dev@gmail.com>
2026-07-31 01:47:04 -0500

    fixed widths/heights between panes and added crossfade

--- src/components/modals/CustomTuningModal.tsx
+++ src/components/modals/CustomTuningModal.tsx
@@ -1,5 +1,5 @@
 import { FocusTrap } from "focus-trap-react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import isEqual from "lodash.isequal";
 import { useState } from "react";
 import { HiOutlineInformationCircle } from "react-icons/hi";
@@ -151,7 +151,7 @@ function CustomTuningModal() {
       >
         <div
           tabIndex={-1}
-          className="baseVertFlex modalGradient relative min-w-[300px] max-w-[95vw] gap-8 rounded-lg border p-4 shadow-sm xl:max-w-[50vw]"
+          className="baseVertFlex modalGradient relative max-w-[95vw] gap-8 rounded-lg border p-4 shadow-sm xl:w-96"
         >
           <div className="baseFlex w-full !justify-between gap-2">
             <div className="baseFlex gap-2">
@@ -196,143 +196,158 @@ function CustomTuningModal() {
             </div>
           </div>
 
-          {inputMode === "auto" ? (
-            <>
-              <div className="baseVertFlex max-w-[23rem] gap-2 rounded-lg border bg-secondary p-2 text-sm shadow-sm sm:max-w-[30rem]">
-                <div className="baseFlex gap-4 px-4 sm:gap-2">
-                  <HiOutlineInformationCircle className="size-4" />
-                  <p className="max-w-[14rem] sm:max-w-[20rem] sm:text-center">
-                    Enter six notes and we will choose valid octaves.
-                  </p>
-                </div>
-                <div className="baseFlex ml-1.5 px-4 text-xs text-foreground/70 sm:ml-0 sm:justify-center">
-                  <p>Leave a space between each note.</p>
-                </div>
-              </div>
-
-              <div className="baseFlex w-72 gap-4">
-                <Input
-                  placeholder="C# B E F# A# E"
-                  value={quickTuningInput.toUpperCase()}
-                  invalid={showInvalidQuickInput}
-                  onChange={(e) => {
-                    const nextValue = e.target.value.toLowerCase();
-                    setQuickTuningInput(nextValue);
-
-                    if (
-                      showInvalidQuickInput &&
-                      normalizeCustomTuningInput(nextValue)
-                    ) {
-                      setShowInvalidQuickInput(false);
-                    }
-                  }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      e.preventDefault();
-                      handleConvertQuickTuningInput();
-                    }
-                  }}
-                />
-
-                <Button
-                  type="button"
-                  disabled={quickTuningInput.length < 6}
-                  variant="secondary"
-                  className="baseFlex h-10 gap-2"
-                  onClick={handleConvertQuickTuningInput}
-                >
-                  <HiOutlineSparkles />
-                  Convert
-                </Button>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="baseVertFlex max-w-[23rem] gap-2 rounded-lg border bg-secondary p-2 text-sm shadow-sm sm:max-w-[30rem]">
-                <div className="baseFlex gap-4 px-4 sm:gap-2">
-                  <HiOutlineInformationCircle className="size-5" />
-                  <p className="max-w-[14rem] sm:max-w-[20rem] sm:text-center">
-                    Scientific Pitch Notation is required here. You can preview
-                    and tweak before saving.
-                  </p>
-                </div>
-                <div className="baseFlex mt-2 gap-2 sm:gap-6">
-                  <div className="baseVertFlex gap-2">
-                    <div className="grid w-full grid-cols-3 place-items-center text-sm font-semibold">
-                      <span>A-G</span>
-                      <span>(#)</span>
-                      <span>0-7 or 1-8</span>
-                    </div>
-                    <div className="grid w-full grid-cols-3 place-items-center text-xs">
-                      <span>Root note</span>
-                      <span className="baseFlex">
-                        Optional sharp
-                        <TooltipProvider delayDuration={150}>
-                          <Tooltip>
-                            <TooltipTrigger>*</TooltipTrigger>
-                            <TooltipContent side={"bottom"}>
-                              <p>B and E notes cannot have sharps.</p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </span>
-                      <span className="baseFlex">
-                        Octave
-                        <TooltipProvider delayDuration={150}>
-                          <Tooltip>
-                            <TooltipTrigger>*</TooltipTrigger>
-                            <TooltipContent side={"bottom"}>
-                              <div className="baseVertFlex z-50 gap-0">
-                                <p>Octaves 0-7 apply to all notes,</p>
-                                <p>but 1-8 apply only to the note C.</p>
-                              </div>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </span>
-                    </div>
+          <AnimatePresence mode="wait" initial={false}>
+            {inputMode === "auto" ? (
+              <motion.div
+                key={"CustomTuningModal-Auto"}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.15 }}
+                className="baseVertFlex h-[170px] w-[332px] gap-8"
+              >
+                <div className="baseVertFlex max-w-[23rem] !items-start gap-2 rounded-lg border bg-secondary p-2 text-sm shadow-sm sm:max-w-[30rem]">
+                  <div className="baseFlex !items-start gap-4 px-4 sm:gap-2">
+                    <HiOutlineInformationCircle className="mt-1 size-5" />
+                    <p className="max-w-[14rem]">
+                      Enter six notes and we will choose valid octaves for you.
+                    </p>
+                  </div>
+                  <div className="baseFlex ml-1.5 px-4 text-xs text-foreground/70 sm:ml-7">
+                    <p>Leave a space between each note.</p>
                   </div>
                 </div>
-              </div>
 
-              <div className="baseFlex gap-1 xs:gap-2">
-                {newTuning.map((value, index) => (
+                <div className="baseFlex w-72 gap-4">
                   <Input
-                    key={index}
-                    placeholder={placeholderNotes[index]}
-                    value={value.toUpperCase()}
-                    invalid={showInvalidInputPerIndex[index]}
-                    style={{
-                      color:
-                        highlightedNoteInputIndex === index
-                          ? "hsl(var(--primary))"
-                          : "hsl(var(--foreground))",
-                    }}
-                    className="w-[52px] p-2 text-center"
+                    placeholder="C# B E F# A# E"
+                    value={quickTuningInput.toUpperCase()}
+                    invalid={showInvalidQuickInput}
                     onChange={(e) => {
-                      if (e.target.value.length > 3) return;
-
-                      const newTuningValues = [...newTuning];
-                      newTuningValues[index] = e.target.value.toLowerCase();
-                      setNewTuning(newTuningValues);
-                      setQuickTuningInput(newTuningValues.join(" "));
+                      const nextValue = e.target.value.toLowerCase();
+                      setQuickTuningInput(nextValue);
 
                       if (
-                        showInvalidInputPerIndex[index] &&
-                        isValidManualTuningInput(newTuningValues[index])
+                        showInvalidQuickInput &&
+                        normalizeCustomTuningInput(nextValue)
                       ) {
-                        setShowInvalidInputPerIndex((invalidInputs) =>
-                          invalidInputs.map((invalid, inputIndex) =>
-                            inputIndex === index ? false : invalid,
-                          ),
-                        );
+                        setShowInvalidQuickInput(false);
+                      }
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        e.preventDefault();
+                        handleConvertQuickTuningInput();
                       }
                     }}
                   />
-                ))}
-              </div>
-            </>
-          )}
+
+                  <Button
+                    type="button"
+                    disabled={quickTuningInput.length < 6}
+                    variant="secondary"
+                    className="baseFlex h-10 gap-2"
+                    onClick={handleConvertQuickTuningInput}
+                  >
+                    <HiOutlineSparkles />
+                    Convert
+                  </Button>
+                </div>
+              </motion.div>
+            ) : (
+              <motion.div
+                key={"CustomTuningModal-Manual"}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.15 }}
+                className="baseVertFlex h-[170px] w-[332px] gap-8"
+              >
+                <div className="baseVertFlex max-w-[23rem] gap-2 rounded-lg border bg-secondary p-2 text-sm shadow-sm sm:max-w-[30rem]">
+                  <div className="baseFlex gap-4 px-4 sm:gap-2">
+                    <HiOutlineInformationCircle className="size-5" />
+                    <p className="max-w-[14rem] sm:max-w-[20rem] sm:text-center">
+                      Scientific Pitch Notation is required here.
+                    </p>
+                  </div>
+                  <div className="baseFlex mt-2 gap-2 sm:gap-6">
+                    <div className="baseVertFlex gap-2">
+                      <div className="grid w-full grid-cols-3 place-items-center text-sm font-semibold">
+                        <span>A-G</span>
+                        <span>(#)</span>
+                        <span>0-7 or 1-8</span>
+                      </div>
+                      <div className="grid w-full grid-cols-3 place-items-center text-xs">
+                        <span>Root note</span>
+                        <span className="baseFlex">
+                          Optional sharp
+                          <TooltipProvider delayDuration={150}>
+                            <Tooltip>
+                              <TooltipTrigger>*</TooltipTrigger>
+                              <TooltipContent side={"bottom"}>
+                                <p>B and E notes cannot have sharps.</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </span>
+                        <span className="baseFlex">
+                          Octave
+                          <TooltipProvider delayDuration={150}>
+                            <Tooltip>
+                              <TooltipTrigger>*</TooltipTrigger>
+                              <TooltipContent side={"bottom"}>
+                                <div className="baseVertFlex z-50 gap-0">
+                                  <p>Octaves 0-7 apply to all notes,</p>
+                                  <p>but 1-8 apply only to the note C.</p>
+                                </div>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="baseFlex gap-1 xs:gap-2">
+                  {newTuning.map((value, index) => (
+                    <Input
+                      key={index}
+                      placeholder={placeholderNotes[index]}
+                      value={value.toUpperCase()}
+                      invalid={showInvalidInputPerIndex[index]}
+                      style={{
+                        color:
+                          highlightedNoteInputIndex === index
+                            ? "hsl(var(--primary))"
+                            : "hsl(var(--foreground))",
+                      }}
+                      className="w-[52px] p-2 text-center"
+                      onChange={(e) => {
+                        if (e.target.value.length > 3) return;
+
+                        const newTuningValues = [...newTuning];
+                        newTuningValues[index] = e.target.value.toLowerCase();
+                        setNewTuning(newTuningValues);
+                        setQuickTuningInput(newTuningValues.join(" "));
+
+                        if (
+                          showInvalidInputPerIndex[index] &&
+                          isValidManualTuningInput(newTuningValues[index])
+                        ) {
+                          setShowInvalidInputPerIndex((invalidInputs) =>
+                            invalidInputs.map((invalid, inputIndex) =>
+                              inputIndex === index ? false : invalid,
+                            ),
+                          );
+                        }
+                      }}
+                    />
+                  ))}
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
 
           <div className="baseFlex w-full !justify-between">
             <Button
